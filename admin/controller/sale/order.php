55c4398844244d47056b6710646769f8ac96c9c4
deependratbg <admin@TBHYDLP-OSF-08.tbipl.com>
2020-05-20 17:52:23 +0530

    changes for quantity

--- admin/controller/sale/order.php
+++ admin/controller/sale/order.php
@@ -2961,7 +2961,7 @@ class ControllerSaleOrder extends Controller {
 
 
             $EditedProducts = $this->model_sale_order->getRealOrderProducts($this->request->get['order_id']);
-
+            //echo '<pre>';print_r($EditedProducts);exit;
 
             if($this->model_sale_order->hasRealOrderProducts($this->request->get['order_id'])) {
 
@@ -3016,11 +3016,15 @@ class ControllerSaleOrder extends Controller {
                     $present = false;
 
                     foreach ($EditedProducts as $EditedProduct) {
+                        if($original_product['product_id'] == $EditedProduct['product_id'] ){
+                            $original_product['quantity_updated'] = $EditedProduct['quantity'];
+                        }
+
                         if(!empty($original_product['name']) && $original_product['name'] == $EditedProduct['name'] && $original_product['unit'] == $EditedProduct['unit'] && $original_product['quantity'] == $EditedProduct['quantity'] ) {
                             $present = true;
                         }
                     }
-
+                    //echo '<pre>';print_r($original_product);exit;
                     if(!$present && !empty($original_product['name'])) {
 
                         $data['difference_products'][] = array(
@@ -3033,6 +3037,7 @@ class ControllerSaleOrder extends Controller {
                             'product_type' => $original_product['product_type'],
                             'model' => $original_product['model'],
                             'option' => $option_data,
+                            'quantity_updated' => $original_product['quantity_updated'],
                             'quantity' => $original_product['quantity'],
                             'price' => $this->currency->format($original_product['price'] + ($this->config->get('config_tax') ? $original_product['tax'] : 0), $order_info['currency_code'], $order_info['currency_value']),
                             'total' => $this->currency->format($original_product['total'] + ($this->config->get('config_tax') ? ($original_product['tax'] * $original_product['quantity']) : 0), $order_info['currency_code'], $order_info['currency_value']),
@@ -3042,7 +3047,7 @@ class ControllerSaleOrder extends Controller {
                     }
 
                 }
-
+                //echo '<pre>';print_r($data['difference_products']);exit;
                 //echo "<pre>";print_r($data['original_products']);die;
 
                 $products = $this->model_sale_order->getRealOrderProducts($this->request->get['order_id']);
@@ -3052,7 +3057,7 @@ class ControllerSaleOrder extends Controller {
                 $products = $this->model_sale_order->getOrderProducts($this->request->get['order_id']);
             }
 
-
+           
             foreach ($products as $product) {
                 $option_data = array();
 
