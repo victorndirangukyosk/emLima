7c827361d18facef7413ea344e41e9bc83072fca
deependratbg <admin@TBHYDLP-OSF-08.tbipl.com>
2020-05-22 11:33:31 +0530

    changes for product count in order info and real order info page

--- front/language/english/account/order.php
+++ front/language/english/account/order.php
@@ -1,1 +1,100 @@
-<?php// Heading$_['heading_title']         = 'Order History';// Text$_['text_placed_on']          = 'Placed on ';$_['text_view']     = 'View';$_['text_items_ordered']       = 'items Ordered';$_['text_real_items_ordered']       = 'real items Ordered';$_['text_refund_text_part1']       = 'Refund (if any) will reflect with in';$_['text_refund_text_part2'] = '48-72 hrs ';$_['text_refund_text_part3']  = 'of delivery';$_['text_delivery_address']  = 'Delivery Address';$_['text_payment']          = 'Payment';$_['text_payment_options']     = 'Payment Options';$_['text_view_billing']       = 'View Address & Billing Details';$_['text_report_issue']       = 'Report an issue';$_['text_load_more']       = 'Load more';$_['text_view_order']       = 'View Order Details';$_['text_go_back']       = 'Go back';$_['text_order_id_with_colon']       = 'Order ID : ';$_['text_items']       = 'items';$_['text_picked']       = 'Picked';$_['text_replaced']     = 'Replaced';$_['text_replacable']     = 'Replacable';$_['text_not_replacable']     = 'Not Replacable';$_['text_replacable_title']     = 'This product can be replaced by shipper with similar product, if not found.';$_['text_not_replacable_title']     = 'Ordered product is to be only delivered.';$_['text_account']          = 'Account';$_['text_order']            = 'Order Information';$_['text_no_order']            = 'Order not found';$_['text_order_detail']     = 'Order Details';$_['text_invoice_no']       = 'Invoice No.:';$_['text_order_id']         = 'Order ID:';$_['text_date_added']       = 'Date Added:';$_['text_shipping_address'] = 'Shipping Address';$_['text_shipping_method']  = 'Shipping Method:';$_['text_payment_address']  = 'Payment Address';$_['text_payment_method']   = 'Payment Method:';$_['text_comment']          = 'Order Comments';$_['text_history']          = 'Order History';$_['text_success']          = 'Success: You have added <b>%s</b> to your <a href="%s">shopping cart</a>!';$_['text_empty']            = 'You have not made any previous orders!';$_['text_error']            = 'The order you requested could not be found!';$_['text_delivered']        = 'Delivered';$_['text_shipped']          = 'Shipped';$_['text_processing']       = 'Processing';$_['text_name']             = 'Name:';$_['text_contact_no']       = 'Contact No:';$_['text_estimated_datetime'] = 'Estimated Delivery Datetime:'; // Column$_['column_order_id']       = 'Order ID';$_['column_product']        = 'No. of Products';$_['column_customer']       = 'Customer';$_['column_name']           = 'Product Name';$_['column_image']           = 'Image';$_['column_unit']           = 'Unit';$_['column_model']          = 'Model';$_['column_quantity']       = 'Quantity';$_['column_price']          = 'Price';$_['column_total']          = 'Total';$_['column_action']         = 'Action';$_['column_date_added']     = 'Date Added';$_['column_status']         = 'Order Status';$_['column_comment']        = 'Comment';// Error$_['error_reorder']         = '%s is not currently available to be reordered.';// Button$_['button_continue']       = 'Continue';$_['text_remaining']          = 'Remaining';$_['text_intransit']            = 'In transist';$_['text_completed']     = 'Completed';$_['text_cancelled']       = 'Canceled';$_['text_cancel']       = 'Cancel';$_['text_delivery_detail']       = 'Delivery';$_['text_no_delivery_alloted']       = 'No driver alloted';$_['text_not_avialable']       = 'Not Available';$_['text_send_rating']       = 'Send Ratings and Review';$_['text_rating']       = 'Rating';$_['text_review']       = 'Review';$_['text_send']       = 'Send';$_['text_real_amount']       = 'Real';$_['text_coupon_willbe_credited']       = 'Coupon cashback will be credited to your account';$_['text_coupon_credited']       = 'Coupon cashback has been credited';$_['cashback_condition']       = 'Cashback Coupons are not part of total'; 
+<?php
+
+// Heading
+$_['heading_title']         = 'Order History';
+
+// Text
+
+$_['text_placed_on']          = 'Placed on ';
+$_['text_view']     = 'View';
+$_['text_items_ordered']       = 'items Ordered';
+$_['text_real_items_ordered']       = 'real items Ordered';
+$_['text_refund_text_part1']       = 'Refund (if any) will reflect with in';
+$_['text_refund_text_part2'] = '48-72 hrs ';
+$_['text_refund_text_part3']  = 'of delivery';
+$_['text_delivery_address']  = 'Delivery Address';
+$_['text_payment']          = 'Payment';
+$_['text_payment_options']     = 'Payment Options';
+$_['text_view_billing']       = 'View Address & Billing Details';
+$_['text_report_issue']       = 'Report an issue';
+$_['text_load_more']       = 'Load more';
+$_['text_view_order']       = 'View Order Details';
+
+$_['text_go_back']       = 'Go back';
+$_['text_order_id_with_colon']       = 'Order ID : ';
+$_['text_items']       = 'items';
+$_['text_products']       = 'Products';
+$_['text_picked']       = 'Picked';
+$_['text_replaced']     = 'Replaced';
+$_['text_replacable']     = 'Replacable';
+$_['text_not_replacable']     = 'Not Replacable';
+
+$_['text_replacable_title']     = 'This product can be replaced by shipper with similar product, if not found.';
+$_['text_not_replacable_title']     = 'Ordered product is to be only delivered.';
+
+$_['text_account']          = 'Account';
+$_['text_order']            = 'Order Information';
+$_['text_no_order']            = 'Order not found';
+$_['text_order_detail']     = 'Order Details';
+$_['text_invoice_no']       = 'Invoice No.:';
+$_['text_order_id']         = 'Order ID:';
+$_['text_date_added']       = 'Date Added:';
+$_['text_shipping_address'] = 'Shipping Address';
+$_['text_shipping_method']  = 'Shipping Method:';
+$_['text_payment_address']  = 'Payment Address';
+$_['text_payment_method']   = 'Payment Method:';
+$_['text_comment']          = 'Order Comments';
+$_['text_history']          = 'Order History';
+$_['text_success']          = 'Success: You have added <b>%s</b> to your <a href="%s">shopping cart</a>!';
+$_['text_empty']            = 'You have not made any previous orders!';
+$_['text_error']            = 'The order you requested could not be found!';
+$_['text_delivered']        = 'Delivered';
+$_['text_shipped']          = 'Shipped';
+$_['text_processing']       = 'Processing';
+$_['text_name']             = 'Name:';
+$_['text_contact_no']       = 'Contact No:';
+$_['text_estimated_datetime'] = 'Estimated Delivery Datetime:';
+ 
+// Column
+$_['column_order_id']       = 'Order ID';
+$_['column_product']        = 'No. of Products';
+$_['column_customer']       = 'Customer';
+$_['column_name']           = 'Product Name';
+$_['column_image']           = 'Image';
+$_['column_unit']           = 'Unit';
+$_['column_model']          = 'Model';
+$_['column_quantity']       = 'Quantity';
+$_['column_price']          = 'Price';
+$_['column_total']          = 'Total';
+$_['column_action']         = 'Action';
+$_['column_date_added']     = 'Date Added';
+$_['column_status']         = 'Order Status';
+$_['column_comment']        = 'Comment';
+
+// Error
+$_['error_reorder']         = '%s is not currently available to be reordered.';
+
+// Button
+$_['button_continue']       = 'Continue';
+
+$_['text_remaining']          = 'Remaining';
+$_['text_intransit']            = 'In transist';
+$_['text_completed']     = 'Completed';
+$_['text_cancelled']       = 'Canceled';
+$_['text_cancel']       = 'Cancel';
+$_['text_delivery_detail']       = 'Delivery';
+
+$_['text_no_delivery_alloted']       = 'No driver alloted';
+$_['text_not_avialable']       = 'Not Available';
+$_['text_send_rating']       = 'Send Ratings and Review';
+
+
+$_['text_rating']       = 'Rating';
+$_['text_review']       = 'Review';
+$_['text_send']       = 'Send';
+$_['text_real_amount']       = 'Real';
+$_['text_coupon_willbe_credited']       = 'Coupon cashback will be credited to your account';
+$_['text_coupon_credited']       = 'Coupon cashback has been credited';
+$_['cashback_condition']       = 'Cashback Coupons are not part of total';
+
+ 
