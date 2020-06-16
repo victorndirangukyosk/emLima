95076d4e480807593679dcce27cc7f79661af3be
sridivyatalluri <sridivya.talluri@technobraingroup,com>
2020-06-16 19:31:57 +0530

    updated items count filter

--- front/controller/common/home.php
+++ front/controller/common/home.php
@@ -866,7 +866,7 @@ class ControllerCommonHome extends Controller {
                 'filter_category_id' => $category['category_id'],
                 'filter_sub_category' => true,
                 'start' => 0,
-                'limit' => ($category['category_id'] == 1359) ? 5 :4,
+                'limit' => ($category['category_id'] == 1359) ? 10 :10,
                 'store_id'=>ACTIVE_STORE_ID
               );
 
