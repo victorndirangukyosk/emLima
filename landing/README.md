904c225b0bee3f9273c53774e7453f32eb1268cf
Mwakisha <barakamwakisha@gmail.com>
2020-07-15 11:10:26 +0300

    Complete navbar, hero section and footer

---
+++ landing/README.md
@@ -0,0 +1,12 @@
+#  KwikBasket Landing Page
+
+Install dependencies
+`npm install`
+
+Run
+
+`gulp`
+
+(gulp should be installed globally `npm install -g gulp`)
+
+
---
+++ landing/public/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * kwikbasket
+ * KwikBasket Landing Page
+ * 
+ * @author KwikBasket
+ * @version 1.0.0
+ * Copyright 2020. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // WRITE YOUR JQUERY HERE
+  });
+
+})(jQuery, window, document);
