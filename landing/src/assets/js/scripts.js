f845e121d2f65f5eb638fb257c5f2b717b338eff
Mwakisha <barakamwakisha@gmail.com>
2020-07-18 15:39:29 +0300

    Add scroll out animations

--- landing/src/assets/js/scripts.js
+++ landing/src/assets/js/scripts.js
@@ -8,12 +8,16 @@
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 
-    $('.nav-link, .navbar-brand').click(function() {
+    $('.nav-link, .navbar-brand').click(function () {
       var sectionTo = $(this).attr('href');
       $('html, body').animate({
         scrollTop: $(sectionTo).offset().top
       }, 700);
-  });
+    });
+
+    ScrollOut({
+      /* options */
+    });    
   });
 
 })(jQuery, window, document);
