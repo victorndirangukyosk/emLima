dc4304d4e9db8d7603fe85e5a2505ab9d711a89a
Mwakisha <barakamwakisha@gmail.com>
2020-07-19 03:13:32 +0300

    Add register page

--- landing/src/assets/js/scripts.js
+++ landing/src/assets/js/scripts.js
@@ -20,7 +20,7 @@
     // Landing page scroll animations
     ScrollOut();
 
-    // Authentication - Login
+    // Customer Login
     $(document).delegate('#login-button', 'click', function (e) {
       e.preventDefault();
 
@@ -60,6 +60,85 @@
           position: 'topRight',
           message: 'Please enter your email and password'
         });
+      }
+    });
+
+    // Customer Registration
+
+    $('#register-phone').keyup(function(e) {
+      if (/\D/g.test(this.value)) {
+        // Filter out non-digits from input value
+        this.value = this.value.replace(/\D/g, '');
+      }
+    });
+
+    $(document).delegate('#register-button', 'click', function (e) {
+      e.preventDefault();
+
+      const registerButton = $('#register-button');
+      const registerForm = $('#register-form')[0];
+      const formIsValid = registerForm.reportValidity();
+
+      if(formIsValid) {
+        const firstName = $('#register-first-name').val();
+        const lastName = $('#register-last-name').val();
+        const email = $('#register-email').val();
+        const phone = $('#register-phone').val();
+        const companyName = $('#register-company-name').val(); 
+        const companyAddress = $('#register-company-address').val();
+        const businessType = $('#register-business-type').val();
+        const buildingName = $('#register-building-name').val();
+        const addressLine = $('#register-address-line').val();
+        const location = $('#register-location').val();
+        const password = $('#register-password').val();
+        const passwordConfirmation = $('#register-password-confirm').val();
+
+        if(passwordConfirmation !== password) {
+          iziToast.warning({
+            position: 'topRight',
+            message: 'Passwords do no match'
+          });
+        } else {
+          registerButton.text('PLEASE WAIT');
+          registerButton.toggleClass('disabled');
+
+          $.ajax({
+            url: 'index.php?path=account/register/register_send_otp',
+            type: 'post',
+            dataType: 'json',
+            data: { 
+              firstname: firstName, 
+              lastname: lastName,
+              email: email,
+              telephone: phone,
+              company_name: companyName,
+              company_address: companyAddress,
+              customer_group_id: businessType,
+              house_building: buildingName,
+              address: addressLine,
+              location: location,
+              password: password,
+              confirm: passwordConfirmation
+            },
+            success: function (json) {
+              if (json['status']) {
+                iziToast.warning({
+                  position: 'topRight',
+                  message: 'Account created successfully'
+                });
+              } else {
+                iziToast.warning({
+                  position: 'topRight',
+                  title: 'Oops',
+                  message: 'We couldn\'t create your account'
+                });
+
+                registerButton.text('SIGN UP');
+                registerButton.toggleClass('disabled');
+              }
+            }
+          });
+        }
       }
     });
   });
