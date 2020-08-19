08fa8146aecc85765c3609851312a969512f387c
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-08-19 14:39:49 +0530

    changes cancel or failed orders

--- front/controller/payment/flutterwave.php
+++ front/controller/payment/flutterwave.php
@@ -245,7 +245,7 @@ class ControllerPaymentFlutterwave extends Controller {
                 $this->model_payment_flutterwave->insertOrderTransactionId($order_id, $transaction_id);
                 $this->model_payment_flutterwavetransactions->addOrderTransaction($transaction['data'], $order_id);
                 $log->write($transaction);
-                $this->response->redirect($this->url->link('checkout/success/orderfailed'));
+                $this->response->redirect($this->url->link('checkout/success'));
             }
 
             if ($this->request->get['status'] == 'cancelled' || $this->request->get['status'] != 'successful') {
