e56935cf7a7febc8911126686fe336999dea2239
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-08-20 10:52:23 +0530

    payment options status

--- front/model/payment/flutterwavepaymentoptions.php
+++ front/model/payment/flutterwavepaymentoptions.php
@@ -3,7 +3,7 @@
 class ModelPaymentFlutterwavepaymentoptions extends Model {
 
     public function getpaymentoptions() {
-        $result = $this->db->query("SELECT * FROM `" . DB_PREFIX . "flutterwave_payment_options`");
+        $result = $this->db->query("SELECT * FROM `" . DB_PREFIX . "flutterwave_payment_options` WHERE `status` = 1");
 
         return $result->rows;
     }
