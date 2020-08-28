d67efcf7dd93f6b6c2b408086598f1cc7558fbf5
Mwakisha <barakamwakisha@gmail.com>
2020-08-28 12:50:03 +0300

    Remove Africa's Talking credentials from config file

--- config.php
+++ config.php
@@ -14,7 +14,3 @@ define('DB_USERNAME', getenv('MYSQL_USERNAME') ? getenv('MYSQL_USERNAME') : 'roo
 define('DB_PASSWORD', getenv('MYSQL_ROOT_PASSWORD') ? getenv('MYSQL_ROOT_PASSWORD') : '');
 define('DB_DATABASE', getenv('MYSQL_ROOT_PASSWORD') ? basename(__DIR__) : 'kwikbasket');
 define('DB_PREFIX', 'hf7_');
-
-// Africa's Talking BulkSMS
-define('AT_USERNAME', 'kwikbasket');
-define('AT_API_KEY', 'c9b0e266179d19ad024476d4c71256cfa1b127c0d26b44e79c6e8438ccc10592');
