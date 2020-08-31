46ffa0c9507a739da51cf827fa1b26c3cee81ac3
sridivyatalluri <sridivya.talluri@technobraingroup.com>
2020-08-31 14:37:09 +0530

    added languae text

--- front/language/english/account/dashboard.php
+++ front/language/english/account/dashboard.php
@@ -18,3 +18,57 @@ $_['text_no_results']  = 'No Results';
 $_['label_name'] = 'Name';
 $_['label_contact_no'] = 'Contact No';
 $_['label_address'] = 'My Address';
+
+
+
+// Heading
+$_['heading_Activitytitle']                = 'Recent Activity';
+
+// Text
+$_['text_customer_address_add']    = '<a href="customer_id=%d">%s</a> added a new address.';
+$_['text_customer_address_edit']   = '<a href="customer_id=%d">%s</a> updated their address.';
+$_['text_customer_address_delete'] = '<a href="customer_id=%d">%s</a> deleted one of their address.';
+$_['text_customer_edit']           = '<a href="customer_id=%d">%s</a> updated their account details.';
+$_['text_customer_forgotten']      = '<a href="customer_id=%d">%s</a> has requested a new password.';
+$_['text_customer_login']          = '<a href="customer_id=%d">%s</a> logged in.';
+$_['text_customer_password']       = '<a href="customer_id=%d">%s</a> updated their account password.';
+$_['text_customer_register']       = '<a href="customer_id=%d">%s</a> registered a new account.';
+$_['text_customer_return_account'] = '<a href="customer_id=%d">%s</a> submitted a product <a href="return_id=%d">return</a>.';
+$_['text_customer_return_guest']  = '%s submitted a product <a href="return_id=%d">return</a>.';
+$_['text_customer_order_account']  = '<a href="customer_id=%d">%s</a> added a <a href="order_id=%d">new order</a>.';
+$_['text_customer_order_guest']    = '%s created a <a href="order_id=%d">new order</a>.';
+$_['text_affiliate_edit']          = '<a href="affiliate_id=%d">%s</a> updated their account details.';
+$_['text_affiliate_forgotten']     = '<a href="affiliate_id=%d">%s</a> requested a new password.';
+$_['text_affiliate_login']         = '<a href="affiliate_id=%d">%s</a> logged in.';
+$_['text_affiliate_password']      = '<a href="affiliate_id=%d">%s</a> updated their account password.';
+$_['text_affiliate_payment']       = '<a href="affiliate_id=%d">%s</a> updated their payment details.';
+$_['text_affiliate_register']      = '<a href="affiliate_id=%d">%s</a> registered for a new account.';
+
+
+
+// Text
+$_['text_list']           = 'Customer Activity List';
+$_['text_address_add']    = '<a href="customer_id=%d">%s</a> added a new address.';
+$_['text_address_edit']   = '<a href="customer_id=%d">%s</a> updated their address.';
+$_['text_address_delete'] = '<a href="customer_id=%d">%s</a> deleted one of their address.';
+$_['text_edit']           = '<a href="customer_id=%d">%s</a> updated their account details.';
+$_['text_forgotten']      = '<a href="customer_id=%d">%s</a> requested a new password.';
+$_['text_login']          = '<a href="customer_id=%d">%s</a> logged in.';
+$_['text_password']       = '<a href="customer_id=%d">%s</a> updated their account password.';
+$_['text_register']       = '<a href="customer_id=%d">%s</a> registered for an account.';
+$_['text_return_account'] = '<a href="customer_id=%d">%s</a> submitted a product return.';
+$_['text_return_guest']   = '%s submitted a product return.';
+$_['text_order_account']  = '<a href="customer_id=%d">%s</a> created a <a href="order_id=%d">new order</a>.';
+$_['text_order_guest']    = '%s created a <a href="order_id=%d">new order</a>.';
+
+// Column
+$_['column_customer']     = 'Customer';
+$_['column_comment']      = 'Comment';
+$_['column_ip']           = 'IP';
+$_['column_date_added']   = 'Date Added';
+
+// Entry
+$_['entry_customer']      = 'Customer';
+$_['entry_ip']            = 'IP';
+$_['entry_date_start']    = 'Date Start';
+$_['entry_date_end']      = 'Date End';
