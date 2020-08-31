99c5db15313a2ae0c8471bce7e9ea7d0ad495e8d
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-08-31 11:04:03 +0530

    when admin creating users sending emails

--- admin/model/sale/customer.php
+++ admin/model/sale/customer.php
@@ -20,8 +20,8 @@ class ModelSaleCustomer extends Model {
         }
 
         if (!empty($data['send_email'])) {
-            /*REMOVED EMAIL SENDING WHEN CREATING USER FROM ADMIN PORTAL*/
-            //$this->sendCustomerRegisterMail($data);
+            /*EMAIL SENDING WHEN CREATING USER FROM ADMIN PORTAL*/
+            $this->sendCustomerRegisterMail($data);
         }
 
         return $customer_id;
