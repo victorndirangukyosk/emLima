03f9b14b4602194c06668ab396f678f77832f347
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-09-01 14:53:53 +0530

    pesapal ipin listener

--- front/controller/payment/pesapal.php
+++ front/controller/payment/pesapal.php
@@ -934,4 +934,72 @@ class ControllerPaymentPesapal extends Controller {
         $this->response->redirect($this->url->link('checkout/success'));
     }
 
+    public function ipinlistener() {
+
+        $log = new Log('error.log');
+        $log->write('ipinlistener');
+        $this->load->model('setting/setting');
+        $this->load->model('payment/pesapal');
+        $this->load->model('checkout/order');
+        $this->load->model('account/customer');
+        $pesapal_creds = $this->model_setting_setting->getSetting('pesapal', 0);
+
+        $consumer_key = $pesapal_creds['pesapal_consumer_key']; //Register a merchant account on
+        //demo.pesapal.com and use the merchant key for testing.
+        //When you are ready to go live make sure you change the key to the live account
+        //registered on www.pesapal.com!
+        $consumer_secret = $pesapal_creds['pesapal_consumer_secret']; // Use the secret from your test
+        //account on demo.pesapal.com. When you are ready to go live make sure you 
+        //change the secret to the live account registered on www.pesapal.com!
+        $statusrequestAPI = 'https://www.pesapal.com/api/querypaymentstatus';
+        //'https://demo.pesapal.com/api/querypaymentstatus'; //change to      
+        //https://www.pesapal.com/api/querypaymentstatus' when you are ready to go live!
+        // Parameters sent to you by PesaPal IPN
+        $pesapalNotification = $this->request->get['pesapal_notification_type'];
+        $pesapalTrackingId = $this->request->get['pesapal_transaction_tracking_id'];
+        $pesapal_merchant_reference = $this->request->get['pesapal_merchant_reference'];
+
+        if ($pesapalNotification == "CHANGE" && $pesapalTrackingId != '') {
+            $log->write('ipinlistener');
+            $token = $params = NULL;
+            $consumer = new OAuthConsumer($consumer_key, $consumer_secret);
+            $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
+
+            //get transaction status
+            $request_status = OAuthRequest::from_consumer_and_token($consumer, $token, "GET", $statusrequestAPI, $params);
+            $request_status->set_parameter("pesapal_merchant_reference", $pesapal_merchant_reference);
+            $request_status->set_parameter("pesapal_transaction_tracking_id", $pesapalTrackingId);
+            $request_status->sign_request($signature_method, $consumer, $token);
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $request_status);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_HEADER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            if (defined('CURL_PROXY_REQUIRED'))
+                if (CURL_PROXY_REQUIRED == 'True') {
+                    $proxy_tunnel_flag = (defined('CURL_PROXY_TUNNEL_FLAG') && strtoupper(CURL_PROXY_TUNNEL_FLAG) == 'FALSE') ? false : true;
+                    curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, $proxy_tunnel_flag);
+                    curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+                    curl_setopt($ch, CURLOPT_PROXY, CURL_PROXY_SERVER_DETAILS);
+                }
+
+            $response = curl_exec($ch);
+            $log->write($response);
+
+            $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+            $raw_header = substr($response, 0, $header_size - 4);
+            $headerArray = explode("\r\n\r\n", $raw_header);
+            $header = $headerArray[count($headerArray) - 1];
+
+            //transaction status
+            $elements = preg_split("/=/", substr($response, $header_size));
+            $status = $elements[1];
+
+            curl_close($ch);
+
+//UPDATE YOUR DB TABLE WITH NEW STATUS FOR TRANSACTION WITH pesapal_transaction_tracking_id $pesapalTrackingId
+        }
+    }
+
 }
