c8adb32fecde5812a6ce84d4c6975f4690b1983b
sridivyatalluri <sridivya.talluri@technobraingroup.com>
2020-09-08 18:06:51 +0530

    forget password API

--- front/controller/api/forgetpassword.php
+++ front/controller/api/forgetpassword.php
@@ -63,4 +63,65 @@ class ControllerApiForgetPassword extends Controller
 
         return !$this->error;
     }
+
+    public function ForgotPassword()
+    {
+        //echo "forget";
+
+        $this->load->model('account/customer');
+
+        $this->load->language('account/forgotten');
+
+        $json['status'] = 501;
+
+        if ($this->validateCustomer()) {
+            $this->load->language('mail/forgotten');
+            $password = substr(sha1(uniqid(mt_rand(), true)), 0, 10);
+            $this->model_account_customer->resetPassword($this->request->post['email'], $password, 1);
+            //1 implies, new password is generated and user need to update his password
+
+            $this->model_account_customer->resetPasswordMail($this->request->post['email'], $password);
+
+            //echo "<pre>";print_r($password);die;
+            $this->session->data['success'] = $this->language->get('text_success');
+
+            $this->model_account_customer->deleteLoginAttempts($this->request->post['email']);
+
+
+             // Add to activity log
+             $customer_info = $this->model_account_customer->getCustomerByEmail($this->request->post['email']);
+
+             if ($customer_info) {
+                 $this->load->model('account/activity');
+ 
+                 $activity_data = [
+                     'customer_id' => $customer_info['customer_id'],
+                     'name' => $customer_info['firstname'].' '.$customer_info['lastname'],
+                 ];
+ 
+                 $this->model_account_activity->addActivity('forgotten', $activity_data);
+             }
+
+            unset($this->session->data['api_id']);
+            $json['success'] = $this->language->get('text_success');
+            $json['status'] = 200;
+        } else {
+            
+            $json['message'] = $this->language->get('error_email');
+        }
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
+    protected function validateCustomer()
+    {
+        if (!isset($this->request->post['email'])) {
+            $this->error['warning'] = $this->language->get('error_email');
+        } elseif (!$this->model_account_customer->getTotalCustomersByEmail($this->request->post['email'])) {
+            $this->error['warning'] = $this->language->get('error_email');
+        }
+
+        return !$this->error;
+    }
 }
