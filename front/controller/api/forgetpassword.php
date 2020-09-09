216f97f82ad5e69bb3b13a29dd5ffbee545b15db
sridivyatalluri <sridivya.talluri@technobraingroup.com>
2020-09-09 15:22:04 +0530

    APIs for change password and forget password

--- front/controller/api/forgetpassword.php
+++ front/controller/api/forgetpassword.php
@@ -72,7 +72,7 @@ class ControllerApiForgetPassword extends Controller
 
         $this->load->language('account/forgotten');
 
-        $json['status'] = 501;
+        $json['status'] = 502;
 
         if ($this->validateCustomer()) {
             $this->load->language('mail/forgotten');
@@ -82,7 +82,7 @@ class ControllerApiForgetPassword extends Controller
 
             $this->model_account_customer->resetPasswordMail($this->request->post['email'], $password);
 
-            //echo "<pre>";print_r($password);die;
+            
             $this->session->data['success'] = $this->language->get('text_success');
 
             $this->model_account_customer->deleteLoginAttempts($this->request->post['email']);
@@ -116,6 +116,8 @@ class ControllerApiForgetPassword extends Controller
 
     protected function validateCustomer()
     {
+
+        
         if (!isset($this->request->post['email'])) {
             $this->error['warning'] = $this->language->get('error_email');
         } elseif (!$this->model_account_customer->getTotalCustomersByEmail($this->request->post['email'])) {
@@ -124,4 +126,93 @@ class ControllerApiForgetPassword extends Controller
 
         return !$this->error;
     }
+
+
+    public function changepassword()
+    {
+        $json = [];
+        $json['success'] = 'Something went wrong!';
+
+        if (!isset($this->session->data['api_id'])) {
+            $json['error'] = $this->language->get('error_permission');
+        } else {
+
+         
+        $this->load->language('account/changepass');
+        $this->load->model('account/changepass');       
+
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->language->get('error_warning_message');
+        } else {
+            $data['error_warning'] = '';
+        }
+
+        if (('POST' == $this->request->server['REQUEST_METHOD']) && $this->validatepasword()) {
+            // echo "<pre>";print_r($this->request->post['newpassword']);die;
+            $this->load->model('account/changepass');
+
+            $result = $this->model_account_changepass->changePassword($this->request->post);
+
+            if (0 == $result) {
+                $json['error'] =  $this->language->get('error_warning_message');
+            } else {
+                $json['success'] =   'Password changed successfully';
+               // $this->response->redirect($this->url->link('account/account', '', 'SSL'));
+
+                // $this->response->redirect($this->url->link('account/changepass/success'));
+            }
+        }
+
+        if (isset($this->error['current'])) {
+            $json['error_current'] = $this->error['current'];
+        } else {
+            $json['error_current'] = '';
+        }
+
+        if (isset($this->error['new'])) {
+            $json['error_new'] = $this->error['new'];
+        } else {
+            $json['error_new'] = '';
+        }
+
+        if (isset($this->error['retype'])) {
+            $json['error_retype'] = $this->error['retype'];
+        } else {
+            $json['error_retype'] = '';
+        }
+
+        }
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
+    public function validatePasword()
+    {
+        // if ((utf8_strlen(trim($this->request->post['currentpassword'])) < 1) || (utf8_strlen(trim($this->request->post['currentpassword'])) > 32)) {
+        //     $this->error['current'] = $this->language->get('error_current');
+        // }
+
+        if ((utf8_strlen(trim($this->request->post['newpassword'])) < 1) || (utf8_strlen(trim($this->request->post['newpassword'])) > 32)) {
+            $this->error['new'] = $this->language->get('error_new');
+        }
+
+        if ((utf8_strlen($this->request->post['retypepassword']) > 96) || ($this->request->post['newpassword'] !== $this->request->post['retypepassword'])) {
+            $this->error['retype'] = $this->language->get('error_retype');
+        }
+
+        // if (empty($this->request->post['currentpassword'])) {
+        //     $this->error['current'] = $this->language->get('error_check');
+        // }
+
+        // if (empty($this->request->post['newpassword'])) {
+        //     $this->error['new'] = $this->language->get('error_new');
+        // }
+
+        // if (empty($this->request->post['retypepassword'])) {
+        //     $this->error['retype'] = $this->language->get('error_retype');
+        // }
+        return !$this->error;
+    }
+
+
 }
--- front/model/account/changepass.php
+++ front/model/account/changepass.php
@@ -39,4 +39,32 @@ class ModelAccountChangepass extends Model
             return 0;
         }
     }
+
+    public function changePassword()
+    {
+        //  echo "Update `" . DB_PREFIX . "user` set password=$this->request->post['newpassword'] where user_id=" . $this->customer->getid();
+
+        // if ($result !== 0) {
+        //    echo "success......";
+        // } else {
+        //     echo "Unsuccessful";
+        // }
+
+        $password = $this->request->post['newpassword'];
+        try {
+            $user_id = $this->request->post['customerid'];
+            //    echo "<pre>";print_r($user_id);die;
+            $this->trigger->fire('pre.customer.edit.password');
+
+            // echo "<pre>";print_r("UPDATE " . DB_PREFIX . "customer SET   temppassword = 0, salt = '" . $this->db->escape($salt = substr(md5(uniqid(rand(), true)), 0, 9)) . "', password = '" . $this->db->escape(sha1($salt . sha1($salt . sha1($password)))) . "' WHERE customer_id = '$user_id '");die;
+
+            $this->db->query('UPDATE '.DB_PREFIX."customer SET   temppassword = 0, salt = '".$this->db->escape($salt = substr(md5(uniqid(rand(), true)), 0, 9))."', password = '".$this->db->escape(sha1($salt.sha1($salt.sha1($password))))."' WHERE customer_id = '$user_id'");
+
+            $this->trigger->fire('post.customer.edit.password');
+
+            return 1;
+        } catch (Exception $e) {
+            return 0;
+        }
+    }
 }
