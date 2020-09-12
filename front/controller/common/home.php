53da2897fce9a3a7174f2ac1d1f3b8a6b24d3bba
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-09-12 15:38:23 +0530

    broken image changes

--- front/controller/common/home.php
+++ front/controller/common/home.php
@@ -1186,14 +1186,9 @@ class ControllerCommonHome extends Controller
             }
             
             $log = new Log('error.log');
-            if ($result['image'] == NULL) {
-                $log->write('IMAGE NULL');
-                $image = $this->model_tool_image->resize('placeholder.png', $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
-            } else if (file_exists(DIR_IMAGE . $result['image'])) {
+            if ($result['image'] != NULL && file_exists(DIR_IMAGE . $result['image'])) {
                 $image = $this->model_tool_image->resize($result['image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
-            } else if (!file_exists(DIR_IMAGE . $result['image'])) {
-                $image = $this->model_tool_image->resize('placeholder.png', $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
-            } else {
+            } else if ($result['image'] == NULL || !file_exists(DIR_IMAGE . $result['image'])) {
                 $image = $this->model_tool_image->resize('placeholder.png', $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
             }
 
