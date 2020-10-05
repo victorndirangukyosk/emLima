640e95e6898f3a390d3576f029150f0599657343
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-10-05 21:07:54 +0530

    order history account managers

--- admin/controller/sale/accountmanageruserorders.php
+++ admin/controller/sale/accountmanageruserorders.php
@@ -2976,4 +2976,51 @@ class ControllerSaleAccountManagerUserOrders extends Controller {
         $this->response->setOutput($this->load->view('sale/edit_order_invoice.tpl', $data));
     }
 
+    public function history() {
+        $this->load->language('sale/order');
+
+        $data['text_no_results'] = $this->language->get('text_no_results');
+
+        $data['column_date_added'] = $this->language->get('column_date_added');
+        $data['column_status'] = $this->language->get('column_status');
+        $data['column_notify'] = $this->language->get('column_notify');
+        $data['column_comment'] = $this->language->get('column_comment');
+
+        if (isset($this->request->get['page'])) {
+            $page = $this->request->get['page'];
+        } else {
+            $page = 1;
+        }
+
+        $data['histories'] = [];
+
+        $this->load->model('sale/order');
+
+        $results = $this->model_sale_order->getOrderHistories($this->request->get['order_id'], ($page - 1) * 10, 10);
+
+        foreach ($results as $result) {
+            $data['histories'][] = [
+                'notify' => $result['notify'] ? $this->language->get('text_yes') : $this->language->get('text_no'),
+                'status' => $result['status'],
+                'order_status_color' => $result['color'],
+                'comment' => nl2br($result['comment']),
+                'date_added' => date($this->language->get('datetime_format'), strtotime($result['date_added'])),
+            ];
+        }
+
+        $history_total = $this->model_sale_order->getTotalOrderHistories($this->request->get['order_id']);
+
+        $pagination = new Pagination();
+        $pagination->total = $history_total;
+        $pagination->page = $page;
+        $pagination->limit = 10;
+        $pagination->url = $this->url->link('sale/order/history', 'token=' . $this->session->data['token'] . '&order_id=' . $this->request->get['order_id'] . '&page={page}', 'SSL');
+
+        $data['pagination'] = $pagination->render();
+
+        $data['results'] = sprintf($this->language->get('text_pagination'), ($history_total) ? (($page - 1) * 10) + 1 : 0, ((($page - 1) * 10) > ($history_total - 10)) ? $history_total : ((($page - 1) * 10) + 10), $history_total, ceil($history_total / 10));
+
+        $this->response->setOutput($this->load->view('sale/order_history.tpl', $data));
+    }
+
 }
