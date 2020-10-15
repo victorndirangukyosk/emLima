edff8b19315113eeeb945d57ee46d5f30e1f0f41
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-10-15 18:01:17 +0530

    email templates

--- system/library/emailtemplate.php
+++ system/library/emailtemplate.php
@@ -447,7 +447,7 @@ class Emailtemplate
     // Customer
     public function getCustomerFind()
     {
-        $result = ['{firstname}', '{lastname}', '{branchname}', '{subuserfirstname}', '{subuserlastname}', '{date}', '{store_name}', '{email}', '{password}', '{account_href}', '{activate_href}', '{order_link}', '{site_url}', '{logo}', '{system_name}', '{year}', '{help_center}', '{white_logo}', '{terms}', '{privacy_policy}', '{system_email}', '{system_phone}', '{amount}', '{transfer_type}', '{ip_address}'];
+        $result = ['{firstname}', '{lastname}', '{branchname}', '{subuserfirstname}', '{subuserlastname}', '{subuserorderid}', '{date}', '{store_name}', '{email}', '{password}', '{account_href}', '{activate_href}', '{order_link}', '{site_url}', '{logo}', '{system_name}', '{year}', '{help_center}', '{white_logo}', '{terms}', '{privacy_policy}', '{system_email}', '{system_phone}', '{amount}', '{transfer_type}', '{ip_address}'];
 
         return $result;
     }
@@ -460,6 +460,7 @@ class Emailtemplate
             'branchname' => $data['branchname'],
             'subuserfirstname' => $data['subuserfirstname'],
             'subuserlastname' => $data['subuserlastname'],
+            'subuserorderid' => $data['subuserorderid'],
             'date' => date($this->language->get('date_format_short'), strtotime(date('Y-m-d H:i:s'))),
             'store_name' => $this->config->get('config_name'),
             'email' => $data['email'],
