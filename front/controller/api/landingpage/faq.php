2eb4ea5cc004178dec2a6affcc43663b06786958
Mwakisha <barakamwakisha@gmail.com>
2020-10-16 16:12:53 +0300

    Update FAQ api

--- front/controller/api/landingpage/faq.php
+++ front/controller/api/landingpage/faq.php
@@ -5,20 +5,26 @@ class ControllerApiLandingpagefaq extends Controller
     private $error = []; 
      
     public function getFaq() {
-        $json = [];
-        // $json['status'] = 200;
-        // $json['data'] = [];
-        // $json['message'] = [];
+        $json['categories'] = [];
+
         $this->load->model('catalog/help');
         $questions = $this->model_catalog_help->getHelps();
         $categories = $this->model_catalog_help->getCategories();
 
         foreach ($categories as $category) {
-            $json[$category['category_id']]['category'] = $category['name'];
-        }
+            $categoryQuestions = [];
 
-        foreach ($questions as $question) {
-            $json[$question['category_id']]['questions'][] = $question;
+            foreach($questions as $question) {
+                if ($question['category_id'] == $category['category_id']) {
+                    $categoryQuestions[] = $question;
+                }
+            }
+
+            $json['categories'][] = [
+                'category_id' => $category['name'],
+                'category' => $category['name'],
+                'questions' => $categoryQuestions
+            ];
         }
 
         $this->response->addHeader('Content-Type: application/json');
