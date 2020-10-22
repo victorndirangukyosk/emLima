1edb7cc8e78a04e4412f258b3661dfc3ae7ecdf5
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-10-22 13:29:59 +0530

    account mananger

--- admin/model/user/accountmanager.php
+++ admin/model/user/accountmanager.php
@@ -41,7 +41,7 @@ class ModelUserAccountmanager extends Model {
     }
 
     public function getUserByEmail($email) {
-        $query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "user WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "'");
+        $query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "user WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "' AND email != ''");
 
         return $query->row;
     }
