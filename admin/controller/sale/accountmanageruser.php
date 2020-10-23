e104414a916ed6ce85316bfdf5a478209323df45
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-10-23 19:10:29 +0530

    account manager auto complete

--- admin/controller/sale/accountmanageruser.php
+++ admin/controller/sale/accountmanageruser.php
@@ -556,5 +556,103 @@ class ControllerSaleAccountManagerUser extends Controller {
             $this->response->setOutput($this->load->view('error/not_found.tpl', $data));
         }
     }
-    
+
+    public function autocompletebyCompany() {
+        $json = [];
+
+        if (isset($this->request->get['filter_name']) || isset($this->request->get['filter_email'])) {
+            if (isset($this->request->get['filter_name'])) {
+                $filter_name = $this->request->get['filter_name'];
+            } else {
+                $filter_name = '';
+            }
+
+            if (isset($this->request->get['filter_company'])) {
+                $filter_company = $this->request->get['filter_company'];
+            } else {
+                $filter_company = '';
+            }
+
+            $this->load->model('sale/customer');
+
+            $filter_data = [
+                'filter_name' => $filter_name,
+                'filter_company' => $filter_company,
+                'start' => 0,
+                'limit' => 5,
+            ];
+
+            $results = $this->model_sale_customer->getCustomers($filter_data);
+
+            foreach ($results as $result) {
+                if ($this->user->isVendor()) {
+                    $result['name'] = $result['firstname'];
+                }
+
+                $json[] = [
+                    'customer_id' => $result['customer_id'],
+                    'customer_group_id' => $result['customer_group_id'],
+                    'name' => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
+                    'customer_group' => $result['customer_group'],
+                    'firstname' => $result['firstname'],
+                    'lastname' => $result['lastname'],
+                    'email' => $result['email'],
+                    'telephone' => $result['telephone'],
+                    'fax' => $result['fax'],
+                    'custom_field' => unserialize($result['custom_field']),
+                    'address' => $this->model_sale_customer->getAddresses($result['customer_id']),
+                ];
+            }
+        }
+
+        $sort_order = [];
+
+        foreach ($json as $key => $value) {
+            $sort_order[$key] = $value['name'];
+        }
+
+        array_multisort($sort_order, SORT_ASC, $json);
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
+    public function autocompletecompany() {
+        $json = [];
+
+        if (isset($this->request->get['filter_name'])) {
+            if (isset($this->request->get['filter_name'])) {
+                $filter_name = $this->request->get['filter_name'];
+            } else {
+                $filter_name = '';
+            }
+
+            $this->load->model('sale/customer');
+
+            $filter_data = [
+                'filter_name' => $filter_name,
+                'start' => 0,
+                'limit' => 5,
+            ];
+
+            $results = $this->model_sale_customer->getCompanies($filter_data);
+            foreach ($results as $result) {
+                $json[] = [
+                    'name' => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
+                ];
+            }
+        }
+
+        $sort_order = [];
+
+        foreach ($json as $key => $value) {
+            $sort_order[$key] = $value['name'];
+        }
+
+        array_multisort($sort_order, SORT_ASC, $json);
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
 }
