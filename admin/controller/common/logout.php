47f30779255766589ffbee43b8b4bf22f3a95206
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-10-24 16:10:36 +0530

    users activity

--- admin/controller/common/logout.php
+++ admin/controller/common/logout.php
@@ -1,13 +1,27 @@
 <?php
 
-class ControllerCommonLogout extends Controller
-{
-    public function index()
-    {
+class ControllerCommonLogout extends Controller {
+
+    public function index() {
         $this->user->logout();
 
         unset($this->session->data['token']);
 
+        $log = new Log('error.log');
+        $this->load->model('user/user_activity');
+
+        $activity_data = [
+            'user_id' => $this->user->getId(),
+            'name' => $this->user->getFirstName() . ' ' . $this->user->getLastName(),
+            'user_group_id' => $this->user->getGroupId(),
+        ];
+        $log->write('user logout');
+
+        $this->model_user_user_activity->addActivity('logout', $activity_data);
+
+        $log->write('user logout');
+
         $this->response->redirect($this->url->link('common/login', '', 'SSL'));
     }
+
 }
