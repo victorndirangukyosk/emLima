64230ec31631b9ad6e4e2915298e17f74ece8ce4
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-10-28 19:48:12 +0530

    account manager customer order

--- admin/model/report/customer.php
+++ admin/model/report/customer.php
@@ -142,6 +142,53 @@ class ModelReportCustomer extends Model
 
         return $query->rows;
     }
+    
+    public function getAccountManagerOrders($data = [])
+    {
+        $sql = "SELECT c.customer_id, CONCAT(c.firstname, ' ', c.lastname) AS customer, c.email, cgd.name AS customer_group, c.status, o.order_id, SUM(op.quantity) as products, SUM(DISTINCT o.total) AS total FROM `".DB_PREFIX.'order` o LEFT JOIN `'.DB_PREFIX.'order_product` op ON (o.order_id = op.order_id)LEFT JOIN `'.DB_PREFIX.'customer` c ON (o.customer_id = c.customer_id) LEFT JOIN `'.DB_PREFIX."customer_group_description` cgd ON (c.customer_group_id = cgd.customer_group_id) WHERE o.customer_id > 0 AND cgd.language_id = '".(int) $this->config->get('config_language_id')."'";
+
+        if (!empty($data['filter_order_status_id'])) {
+            $sql .= " AND o.order_status_id = '".(int) $data['filter_order_status_id']."'";
+        } else {
+            $sql .= " AND o.order_status_id > '0'";
+        }
+        
+        if ($this->user->isAccountManager()) {
+            $sql .= " AND c.account_manager_id = '" . (int) $this->user->isAccountManager() . "'";
+        }
+
+        if (!empty($data['filter_date_start'])) {
+            $sql .= " AND DATE(o.date_added) >= '".$this->db->escape($data['filter_date_start'])."'";
+        }
+
+        if (!empty($data['filter_date_end'])) {
+            $sql .= " AND DATE(o.date_added) <= '".$this->db->escape($data['filter_date_end'])."'";
+        }
+
+        if (!empty($data['filter_customer'])) {
+            $sql .= " AND CONCAT(c.firstname, ' ', c.lastname) LIKE '".$this->db->escape($data['filter_customer'])."'";
+        }
+
+        $sql .= ' GROUP BY o.order_id';
+
+        $sql = 'SELECT t.customer_id, t.customer, t.email, t.customer_group, t.status, COUNT(DISTINCT t.order_id) AS orders, SUM(t.products) AS products, SUM(t.total) AS total FROM ('.$sql.') AS t GROUP BY t.customer_id ORDER BY total DESC';
+
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) {
+                $data['start'] = 0;
+            }
+
+            if ($data['limit'] < 1) {
+                $data['limit'] = 20;
+            }
+
+            $sql .= ' LIMIT '.(int) $data['start'].','.(int) $data['limit'];
+        }
+
+        $query = $this->db->query($sql);
+
+        return $query->rows;
+    }
 
     public function getOrdersOld($data = [])
     {
@@ -188,6 +235,33 @@ class ModelReportCustomer extends Model
             $sql .= " AND o.order_status_id = '".(int) $data['filter_order_status_id']."'";
         } else {
             $sql .= " AND o.order_status_id > '0'";
+        }
+
+        if (!empty($data['filter_date_start'])) {
+            $sql .= " AND DATE(o.date_added) >= '".$this->db->escape($data['filter_date_start'])."'";
+        }
+
+        if (!empty($data['filter_date_end'])) {
+            $sql .= " AND DATE(o.date_added) <= '".$this->db->escape($data['filter_date_end'])."'";
+        }
+
+        $query = $this->db->query($sql);
+
+        return $query->row['total'];
+    }
+    
+    public function getAccountManagerTotalOrders($data = [])
+    {
+        $sql = 'SELECT COUNT(DISTINCT o.customer_id) AS total FROM `'.DB_PREFIX."order` o WHERE o.customer_id > '0'";
+
+        if (!empty($data['filter_order_status_id'])) {
+            $sql .= " AND o.order_status_id = '".(int) $data['filter_order_status_id']."'";
+        } else {
+            $sql .= " AND o.order_status_id > '0'";
+        }
+        
+        if ($this->user->isAccountManager()) {
+            $sql .= " AND c.account_manager_id = '" . (int) $this->user->isAccountManager() . "'";
         }
 
         if (!empty($data['filter_date_start'])) {
