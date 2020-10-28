ee1cd450865f4d59a4c8fc991b7b74fe10ef312a
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-10-29 00:41:51 +0530

    online users

--- front/model/tool/online.php
+++ front/model/tool/online.php
@@ -1,19 +1,22 @@
 <?php
 
-class ModelToolOnline extends Model
-{
-    public function whosonline($ip, $customer_id, $url, $referer)
-    {   
+class ModelToolOnline extends Model {
+
+    public function whosonline($ip, $customer_id, $url, $referer) {
         $log = new Log('error.log');
         $log->write('whosonline');
         $log->write($customer_id);
         $log->write('whosonline');
-        $this->db->query('DELETE FROM `'.DB_PREFIX."customer_online` WHERE date_added < '".date('Y-m-d H:i:s', strtotime('-1 hour'))."'");
+        $this->db->query('DELETE FROM `' . DB_PREFIX . "customer_online` WHERE date_added < '" . date('Y-m-d H:i:s', strtotime('-1 hour')) . "'");
 
-        $query = $this->db->query('SELECT COUNT(*) AS total FROM `'.DB_PREFIX."customer_online` WHERE `ip` = '".$this->db->escape($ip)."'");
+        $query = $this->db->query('SELECT COUNT(*) AS total FROM `' . DB_PREFIX . "customer_online` WHERE `ip` = '" . $this->db->escape($ip) . "'");
 
         if (!$query->row['total']) {
-            $this->db->query('REPLACE INTO `'.DB_PREFIX."customer_online` SET `ip` = '".$this->db->escape($ip)."', `customer_id` = '".(int) $customer_id."', `url` = '".$this->db->escape($url)."', `referer` = '".$this->db->escape($referer)."', `date_added` = '".$this->db->escape(date('Y-m-d H:i:s'))."'");
+            $log->write('REPLACE whosonline');
+            $log->write($customer_id);
+            $log->write('REPLACE whosonline');
+            $this->db->query('REPLACE INTO `' . DB_PREFIX . "customer_online` SET `ip` = '" . $this->db->escape($ip) . "', `customer_id` = '" . (int) $customer_id . "', `url` = '" . $this->db->escape($url) . "', `referer` = '" . $this->db->escape($referer) . "', `date_added` = '" . $this->db->escape(date('Y-m-d H:i:s')) . "'");
         }
     }
+
 }
