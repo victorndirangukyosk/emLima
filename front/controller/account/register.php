eca78d88eb17961e9a3172f29ff55e56084c94fe
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-10-31 15:56:06 +0530

    registration changes

--- front/controller/account/register.php
+++ front/controller/account/register.php
@@ -627,7 +627,7 @@ class ControllerAccountRegister extends Controller
                     $subject = $this->emailtemplate->getSubject('registerOTP', 'registerotp_2', $data);
                     $message = $this->emailtemplate->getMessage('registerOTP', 'registerotp_2', $data);
                     $log = new Log('error.log');
-                    $log = $log->write('registerOTP');
+                    $log = $log->write($subject);
 
                     $mail = new mail($this->config->get('config_mail'));
                     $mail->setTo($this->request->post['email']);
