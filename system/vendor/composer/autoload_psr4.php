871f8280fbc919a6e1a444be421b6122dc3739ee
Mwakisha <barakamwakisha@gmail.com>
2020-11-09 11:52:29 +0300

    Add aws php sdk

--- system/vendor/composer/autoload_psr4.php
+++ system/vendor/composer/autoload_psr4.php
@@ -14,4 +14,5 @@ return array(
     'Joomla\\Cache\\' => array($vendorDir . '/joomla/cache/src'),
     'AfricasTalking\\SDK\\' => array($vendorDir . '/africastalking/africastalking/src'),
     'libphonenumber\\' => array($vendorDir . '/giggsey/libphonenumber-for-php/src'),
+    'Aws\\' => array($vendorDir . '/aws-sdk-php/src'),
 );
