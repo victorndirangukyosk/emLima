f4402c74f3e2feeaa87b7f68cbe036579a21de27
Mwakisha <barakamwakisha@gmail.com>
2020-11-11 11:51:24 +0300

    WIP: Implement exporting logs to CloudWatch

--- system/library/log.php
+++ system/library/log.php
@@ -1,21 +1,48 @@
 <?php
 
+require_once DIR_SYSTEM.'/vendor/aws-sdk-php/vendor/autoload.php';
+
+use Aws\CloudWatchLogs\CloudWatchLogsClient;
+
 class Log
 {
-    private $handle;
+    private $cloudWatchClient;
 
     public function __construct($filename)
     {
-        $this->handle = fopen(DIR_LOG.date('Y-m-d').'.log', 'a');
+        $this->cloudWatchClient = new CloudWatchLogsClient([
+            'version'     => 'latest',
+            'region'      => AWS_CLOUDWATCH_LOGS_REGION,
+            'credentials' => [
+                'key'    => AWS_ACCESS_KEY,
+                'secret' => AWS_SECRET_ACCESS_KEY,
+            ],
+        ]);
     }
 
     public function write($message)
     {
-        fwrite($this->handle, date('Y-m-d G:i:s').' - '.print_r($message, true)."\n");
-    }
+        // The message passed to the CloudWatch client must be non null
+        if(strlen($message) == 0) {
+            $message = "Empty log message";
+        }
 
-    public function __destruct()
-    {
-        fclose($this->handle);
+        $this->cloudWatchClient->putLogEventsAsync([
+            'logEvents' => [
+                [
+                    'message' => $message, 
+                    'timestamp' => time(),
+                ],
+            ],
+            'logGroupName' => AWS_CLOUDWATCH_LOGS_GROUP,
+            'logStreamName' => AWS_CLOUDWATCH_LOGS_STREAM,
+        ])->then(
+            function($response) {
+                echo $response."<br>";
+            },
+            function($error) {
+                echo $error."<br>";
+            }
+        );
     }
 }
