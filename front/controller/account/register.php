365ef9a25b1d09cce7e62c85e3a039a9f86a68d4
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2020-11-19 12:13:15 +0530

    customer registration source

--- front/controller/account/register.php
+++ front/controller/account/register.php
@@ -830,7 +830,7 @@ class ControllerAccountRegister extends Controller
                     } else {
                         $this->request->post['dob'] = null;
                     }
-
+                    $this->request->post['source'] = 'WEB';
                     //$this->request->post['password'] = mt_rand(1000,9999);
 
                     $customer_id = $this->model_account_customer->addCustomer($this->request->post, true);
