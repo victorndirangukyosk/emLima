2aac80d661f86320df387a70d2cf30ec739a7bbe
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2021-03-08 19:54:31 +0530

    download pdf mpdf

--- admin/controller/sale/order.php
+++ admin/controller/sale/order.php
@@ -4977,10 +4977,10 @@ class ControllerSaleOrder extends Controller {
           $stylesheet = file_get_contents(DIR_TEMPLATE.'pdf_css/bootstrap.min.css');
           $stylesheet2 = file_get_contents(DIR_TEMPLATE.'pdf_css/font-awesome.min.css');
           $stylesheet3 = file_get_contents(DIR_TEMPLATE.'pdf_css/print.css');
-        //   $mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
-        //   $mpdf->WriteHTML($stylesheet2,\Mpdf\HTMLParserMode::HEADER_CSS);
-        //   $mpdf->WriteHTML($stylesheet3,\Mpdf\HTMLParserMode::HEADER_CSS);
-        //   $mpdf->WriteHTML($this->load->view('sale/order_invoice_mpdf.tpl', $data['orders'][0]),\Mpdf\HTMLParserMode::HTML_BODY);
+          $mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
+          $mpdf->WriteHTML($stylesheet2,\Mpdf\HTMLParserMode::HEADER_CSS);
+          $mpdf->WriteHTML($stylesheet3,\Mpdf\HTMLParserMode::HEADER_CSS);
+          $mpdf->WriteHTML($this->load->view('sale/order_invoice_mpdf.tpl', $data['orders'][0]),\Mpdf\HTMLParserMode::HTML_BODY);
           $mpdf->WriteHTML($this->load->view('sale/order_invoice_mpdf.tpl', $data['orders'][0]));
           $mpdf->Output();
           $mpdf->Output("KwikBasket Invoice # ".$order_id.".pdf", 'D');  
