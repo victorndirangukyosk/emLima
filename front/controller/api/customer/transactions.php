582715c0b6728965762451f71496744d5e9d39e1
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2021-04-12 15:19:20 +0530

    customer transactions api

--- front/controller/api/customer/transactions.php
+++ front/controller/api/customer/transactions.php
@@ -10,11 +10,15 @@ class ControllerApiCustomerTransactions extends Controller {
         $json['data'] = [];
         $json['message'] = [];
 
-        $page = 1;
+        if (!empty($this->request->get['page'])) {
+            $page = $this->request->get['page'];
+        } else {
+            $page = 1;
+        }
         $log = new Log('error.log');
         $log->write('getAllOrders');
         $this->load->model('account/order');
-        $results_orders = $this->model_account_order->getOrders(($page - 1) * 10, 10, $NoLimit = true);
+        $results_orders = $this->model_account_order->getOrders(($page - 1) * 10, 10, $NoLimit = false);
         $json['data'] = $results_orders;
         $this->response->addHeader('Content-Type: application/json');
         $this->response->setOutput(json_encode($json));
