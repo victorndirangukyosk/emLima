ed5ed2f5ad8a5020366a00cf24027a5500bc5abc
sridivyatalluri <sridivya.talluri@technobraingroup.com>
2021-04-30 16:24:48 +0530

    Update dbupdates.php

--- admin/model/scheduler/dbupdates.php
+++ admin/model/scheduler/dbupdates.php
@@ -33,4 +33,28 @@ class ModelSchedulerDbupdates extends Model {
 
     }
 
+
+    public function addProcessingOrderHistory($order_id, $order_status_id, $comment = '', $notify = true, $added_by = '', $added_by_role = '') 
+    {
+        $log = new Log('error.log'); 
+        $this->trigger->fire('pre.order.history.add', $order_id);
+        try{
+                $order_info = $this->getOrder($order_id);       
+                
+                $this->db->query("INSERT INTO " . DB_PREFIX . "order_history SET order_id = '" . (int) $order_id . "', added_by = '" . (int) $added_by . "', role = '" . $added_by_role . "', order_status_id = '" . (int) $order_status_id . "', notify = '" . (int) $notify . "', comment = '" . $this->db->escape($comment) . "', date_added = NOW()");
+            
+                // Stock subtraction
+                $order_product_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "order_product WHERE order_id = '" . (int) $order_id . "'");
+                foreach ($order_product_query->rows as $order_product) {
+                    $this->db->query("UPDATE " . DB_PREFIX . "product_to_store SET quantity = (quantity - " . (float) $order_product['quantity'] . ") WHERE product_store_id = '" . (int) $order_product['product_id'] . "' AND subtract_quantity = '1'");
+                }
+            }
+            catch(exception $ex)
+            {
+                $log->write('addProcessingOrderHistory -error');
+            }
+
+         return true;   
+    }
+
 }
