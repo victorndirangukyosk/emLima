bc5fbff6fbb8a71cc259967d646b9cb7e7720d2e
sridivyatalluri <sridivya.talluri@technobraingroup.com>
2021-05-07 13:11:37 +0530

    no change

--- admin/controller/sale/order.php
+++ admin/controller/sale/order.php
@@ -3636,7 +3636,7 @@ class ControllerSaleOrder extends Controller
 
             $data['header'] = $this->load->controller('common/header');
             $data['column_left'] = $this->load->controller('common/column_left');
-            $data['footer'] = $this->load->controller('common/footer');
+            $data['footer'] = $this->load->controller('common/footer'); 
 
             $this->response->setOutput($this->load->view('sale/order_info.tpl', $data));
         } else {
