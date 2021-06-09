845f6f421cd828a6015f6defd73031692978b53c
ramakanthrapaka-technobraingroup <ramakanth.rapaka@technobraingroup.com>
2021-06-09 19:43:34 +0530

    interswitch

--- front/controller/payment/interswitch.php
+++ front/controller/payment/interswitch.php
@@ -4,7 +4,35 @@ class ControllerPaymentInterswitch extends Controller {
 
     public function index() {
 
-        $data = [];
+        $this->load->language('payment/pesapal');
+        $this->load->model('setting/setting');
+        $this->load->model('payment/interswitch');
+        $this->load->model('checkout/order');
+        $this->load->model('account/customer');
+
+        foreach ($this->session->data['order_id'] as $key => $value) {
+            $order_id = $value;
+        }
+
+        $order_info = $this->model_checkout_order->getOrder($order_id);
+        $customer_info = $this->model_account_customer->getCustomer($order_info['customer_id']);
+
+        if (count($order_info) > 0) {
+            $amount = (int) ($order_info['total']);
+        }
+
+        $data['customer_number'] = $this->customer->getTelephone();
+
+        $interswitch_creds = $this->model_setting_setting->getSetting('interswitch', 0);
+        $data['interswitch_merchant_code'] = $interswitch_creds['interswitch_merchant_code'];
+        $data['interswitch_pay_item_id'] = $interswitch_creds['interswitch_pay_item_id'];
+        $data['interswitch_data_ref'] = base64_encode($order_info['customer_id'] . '_' . $order_id . '_' . $amount . '_' . date("Y-m-d h:i:s"));
+        $data['interswitch_customer_id'] = $customer_info['customer_id'];
+        $data['interswitch_customer_name'] = $customer_info['firstname'] . ' ' . $customer_info['lastname'];
+        $data['interswitch_amount'] = $amount;
+        $log = new Log('error.log');
+        $log->write($interswitch_creds['interswitch_merchant_code']);
+
         if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/interswitch.tpl')) {
             return $this->load->view($this->config->get('config_template') . '/template/payment/interswitch.tpl', $data);
         } else {
--- front/ui/theme/default/template/payment/interswitch.tpl
+++ front/ui/theme/default/template/payment/interswitch.tpl
@@ -5,33 +5,33 @@
         <div class="form-input">
             <label>Merchant Code</label>
             <!-- <input id="param-merchantCode" value="MX8067" required /> -->
-            <input id="param-merchantCode" required="" />
+            <input id="param-merchantCode" required="" value="<?=$interswitch_merchant_code; ?>" />
         </div>
 
         <div class="form-input">
             <label>Item Id</label>
             <!-- <input id="param-itemId" value="3634738" required /> -->
-            <input id="param-itemId" required="" />
+            <input id="param-itemId" required="" value="<?=$interswitch_pay_item_id; ?>" />
         </div>
 
         <div class="form-input">
             <label>Transaction Reference</label>
-            <input id="param-transRef" required="" />
+            <input id="param-transRef" required="" value="<?=$interswitch_data_ref; ?>" />
         </div>
 
         <div class="form-input">
             <label>Amount</label>
-            <input id="param-amount" required="" value="1000" />
+            <input id="param-amount" required="" value="<?=$interswitch_amount; ?>" />
         </div>
 
         <div class="form-input">
             <label>Customer Name(Optional)</label>
-            <input id="param-customerName" />
+            <input id="param-customerName" value="<?=$interswitch_customer_name; ?>" />
         </div>
 
         <div class="form-input">
             <label>Customer Id(Optional)</label>
-            <input id="param-customerId" />
+            <input id="param-customerId" value="<?=$interswitch_customer_id; ?>" />
         </div>
 
         <div class="form-input">
