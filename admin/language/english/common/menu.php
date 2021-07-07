3b124625b4855a0699c64fa137d97cb93eaae816
Sri Divya <sridivya.talluri@technobraingroup.com>
2021-07-07 16:45:23 +0530

    Menu Item added

--- admin/language/english/common/menu.php
+++ admin/language/english/common/menu.php
@@ -18,6 +18,7 @@ $_['text_farmer'] = 'Farmers';
 $_['text_farmertransactions'] = 'Farmer Transactions';
 $_['text_customer_group'] = 'Customer Groups';
 $_['text_customer_ban_ip'] = 'Banned IP';
+$_['text_customer_feedback'] = 'Customer Feedback';
 $_['text_account_managers'] = 'Account Managers';
 $_['text_account_managers_customers'] = 'Customers';
 $_['text_custom_field'] = 'Custom Fields';
