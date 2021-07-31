1ca1c50de6fe69d00df13df4c7551a2aa8634dde
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-08-01 00:57:15 +0530

    interswitch changes in mobile apis

--- front/controller/api/customer/payment.php
+++ front/controller/api/customer/payment.php
@@ -429,14 +429,4 @@ class Controllerapicustomerpayment extends Controller
         }
         echo $status;
     }
-    
-    public function addInterswitchpaymentresponse() {
-        
-        $json = [];
-        $json['status'] = 200;
-        $json['message'] = [];
-        $json['data'] = $this->request->post;
-        $this->response->addHeader('Content-Type: application/json');
-        $this->response->setOutput(json_encode($json));
-    }
 }
