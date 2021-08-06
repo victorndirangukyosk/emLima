082e1050f0bceb506e8cb865d2f8039680363649
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-08-06 15:25:51 +0530

    customer experince

--- admin/model/user/customerexperience.php
+++ admin/model/user/customerexperience.php
@@ -404,12 +404,12 @@ class ModelUserCustomerexperience extends Model {
         return $query->rows;
     }
 
-    public function AssignCustomersToAccountManager($customer_id, $account_manager_id) {
-        $this->db->query('UPDATE `' . DB_PREFIX . "customer` SET account_manager_id = '" . (int) $account_manager_id . "' WHERE customer_id = '" . (int) $customer_id . "'");
+    public function AssignCustomersToCustomerExperience($customer_id, $customer_experience_id) {
+        $this->db->query('UPDATE `' . DB_PREFIX . "customer` SET account_manager_id = '" . (int) $customer_experience_id . "' WHERE customer_id = '" . (int) $customer_id . "'");
     }
 
-    public function UnAssignCustomersToAccountManager($customer_id, $account_manager_id) {
-        $this->db->query('UPDATE `' . DB_PREFIX . "customer` SET account_manager_id = NULL WHERE customer_id = '" . (int) $customer_id . "' AND account_manager_id = '" . (int) $account_manager_id . "'");
+    public function UnAssignCustomersToCustomerExperience($customer_id, $customer_experience_id) {
+        $this->db->query('UPDATE `' . DB_PREFIX . "customer` SET account_manager_id = NULL WHERE customer_id = '" . (int) $customer_id . "' AND account_manager_id = '" . (int) $customer_experience_id . "'");
     }
 
     public function getCustomerByCustomerExperienceId($customer_experience_id) {
@@ -422,8 +422,8 @@ class ModelUserCustomerexperience extends Model {
         return $query->rows;
     }
 
-    public function getTotalAssignedCustomers($account_manager_id) {
-        $query = $this->db->query('SELECT COUNT(*) AS total FROM ' . DB_PREFIX . "customer WHERE account_manager_id = '" . (int) $account_manager_id . "'");
+    public function getTotalAssignedCustomers($customer_experience_id) {
+        $query = $this->db->query('SELECT COUNT(*) AS total FROM ' . DB_PREFIX . "customer WHERE customer_experience_id = '" . (int) $customer_experience_id . "'");
 
         return $query->row['total'];
     }
