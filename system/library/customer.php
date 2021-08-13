7919842118324f332f4d0bbdc923ef87d1e609ef
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-08-13 21:00:55 +0530

    set customer category when login

--- system/library/customer.php
+++ system/library/customer.php
@@ -1,7 +1,7 @@
 <?php
 
-class Customer
-{
+class Customer {
+
     private $customer_id;
     private $firstname;
     private $lastname;
@@ -16,19 +16,34 @@ class Customer
     private $mobile_notification;
     private $email_notification;
     private $payment_terms;
+    private $customer_category;
 
-
-    public function __construct($registry)
-    {
+    public function __construct($registry) {
         $this->config = $registry->get('config');
         $this->db = $registry->get('db');
         $this->request = $registry->get('request');
         $this->session = $registry->get('session');
 
         if (isset($this->session->data['customer_id'])) {
-            $customer_query = $this->db->query('SELECT * FROM '.DB_PREFIX."customer WHERE customer_id = '".(int) $this->session->data['customer_id']."' AND status = '1'");
+            $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE customer_id = '" . (int) $this->session->data['customer_id'] . "' AND status = '1'");
 
             if ($customer_query->num_rows) {
+
+                /* SET CUSTOMER CATEGORY */
+                if ($customer_query->row['customer_id'] > 0 && $customer_query->row['parent'] > 0) {
+                    $parent_customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE customer_id = '" . $this->db->escape($customer_query->row['parent']) . "' AND status = '1' AND approved='1'");
+                    if ($customer_query->num_rows > 0 && $parent_customer_query->row['customer_id'] > 0) {
+                        $this->customer_category = $parent_customer_query->row['customer_category'];
+                    } else {
+                        $this->customer_category = NULL;
+                    }
+                }
+
+                if ($customer_query->row['customer_id'] > 0 && $customer_query->row['parent'] == NULL) {
+                    $this->customer_category = $customer_query->row['customer_category'];
+                }
+                /* SET CUSTOMER CATEGORY */
+
                 $this->customer_id = $customer_query->row['customer_id'];
                 $this->firstname = $customer_query->row['firstname'];
                 $this->lastname = $customer_query->row['lastname'];
@@ -44,13 +59,12 @@ class Customer
                 $this->email_notification = $customer_query->row['email_notification'];
                 $this->payment_terms = $customer_query->row['payment_terms'];
 
+                $this->db->query('UPDATE ' . DB_PREFIX . "customer SET cart = '" . $this->db->escape(isset($this->session->data['cart']) ? serialize($this->session->data['cart']) : '') . "', wishlist = '" . $this->db->escape(isset($this->session->data['wishlist']) ? serialize($this->session->data['wishlist']) : '') . "', ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "' WHERE customer_id = '" . (int) $this->customer_id . "'");
 
-                $this->db->query('UPDATE '.DB_PREFIX."customer SET cart = '".$this->db->escape(isset($this->session->data['cart']) ? serialize($this->session->data['cart']) : '')."', wishlist = '".$this->db->escape(isset($this->session->data['wishlist']) ? serialize($this->session->data['wishlist']) : '')."', ip = '".$this->db->escape($this->request->server['REMOTE_ADDR'])."' WHERE customer_id = '".(int) $this->customer_id."'");
-
-                $query = $this->db->query('SELECT * FROM '.DB_PREFIX."customer_ip WHERE customer_id = '".(int) $this->session->data['customer_id']."' AND ip = '".$this->db->escape($this->request->server['REMOTE_ADDR'])."'");
+                $query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer_ip WHERE customer_id = '" . (int) $this->session->data['customer_id'] . "' AND ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "'");
 
                 if (!$query->num_rows) {
-                    $this->db->query('INSERT INTO '.DB_PREFIX."customer_ip SET customer_id = '".(int) $this->session->data['customer_id']."', ip = '".$this->db->escape($this->request->server['REMOTE_ADDR'])."', date_added = NOW()");
+                    $this->db->query('INSERT INTO ' . DB_PREFIX . "customer_ip SET customer_id = '" . (int) $this->session->data['customer_id'] . "', ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "', date_added = NOW()");
                 }
             } else {
                 $this->logout();
@@ -58,12 +72,11 @@ class Customer
         }
     }
 
-    public function login($email, $password, $override = false)
-    {
+    public function login($email, $password, $override = false) {
         if ($override) {
-            $customer_query = $this->db->query('SELECT * FROM '.DB_PREFIX."customer WHERE LOWER(email) = '".$this->db->escape(utf8_strtolower($email))."' AND status = '1'");
+            $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "' AND status = '1'");
         } else {
-            $customer_query = $this->db->query('SELECT * FROM '.DB_PREFIX."customer WHERE LOWER(email) = '".$this->db->escape(utf8_strtolower($email))."' AND (password = SHA1(CONCAT(salt, SHA1(CONCAT(salt, SHA1('".$this->db->escape($password)."'))))) OR password = '".$this->db->escape(md5($password))."') AND status = '1' AND approved = '1'");
+            $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "' AND (password = SHA1(CONCAT(salt, SHA1(CONCAT(salt, SHA1('" . $this->db->escape($password) . "'))))) OR password = '" . $this->db->escape(md5($password)) . "') AND status = '1' AND approved = '1'");
         }
 
         if ($customer_query->num_rows) {
@@ -72,17 +85,31 @@ class Customer
             $log = new Log('error.log');
             $log->write('FROM HERE PARENT CUSTOMER SESSION ASSIGN system_library_customer.php login');
 
-            /*if ($customer_query->row['cart'] && is_string($customer_query->row['cart'])) {
-                $cart = unserialize($customer_query->row['cart']);
-
-                foreach ($cart as $key => $value) {
-                    if (!array_key_exists($key, $this->session->data['cart'])) {
-                        $this->session->data['cart'][$key] = $value;
-                    } else {
-                        $this->session->data['cart'][$key] += $value;
-                    }
+            /* SET CUSTOMER CATEGORY */
+            if ($customer_query->row['customer_id'] > 0 && $customer_query->row['parent'] > 0) {
+                $parent_customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE customer_id = '" . $this->db->escape($customer_query->row['parent']) . "' AND status = '1' AND approved='1'");
+                if ($customer_query->num_rows > 0 && $parent_customer_query->row['customer_id'] > 0) {
+                    $this->customer_category = $parent_customer_query->row['customer_category'];
+                } else {
+                    $this->customer_category = NULL;
                 }
-            }*/
+            }
+
+            if ($customer_query->row['customer_id'] > 0 && $customer_query->row['parent'] == NULL) {
+                $this->customer_category = $customer_query->row['customer_category'];
+            }
+            /* SET CUSTOMER CATEGORY */
+            /* if ($customer_query->row['cart'] && is_string($customer_query->row['cart'])) {
+              $cart = unserialize($customer_query->row['cart']);
+
+              foreach ($cart as $key => $value) {
+              if (!array_key_exists($key, $this->session->data['cart'])) {
+              $this->session->data['cart'][$key] = $value;
+              } else {
+              $this->session->data['cart'][$key] += $value;
+              }
+              }
+              } */
 
             if ($customer_query->row['wishlist'] && is_string($customer_query->row['wishlist'])) {
                 if (!isset($this->session->data['wishlist'])) {
@@ -112,7 +139,7 @@ class Customer
             $this->email_notification = $customer_query->row['email_notification'];
             $this->payment_terms = $customer_query->row['payment_terms'];
 
-            $this->db->query('UPDATE '.DB_PREFIX."customer SET ip = '".$this->db->escape($this->request->server['REMOTE_ADDR'])."' WHERE customer_id = '".(int) $this->customer_id."'");
+            $this->db->query('UPDATE ' . DB_PREFIX . "customer SET ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "' WHERE customer_id = '" . (int) $this->customer_id . "'");
 
             return true;
         } else {
@@ -120,12 +147,11 @@ class Customer
         }
     }
 
-    public function loginByPhone($customer_id, $override = true)
-    {
+    public function loginByPhone($customer_id, $override = true) {
         if ($override) {
-            $customer_query = $this->db->query('SELECT * FROM '.DB_PREFIX."customer WHERE customer_id = '".$this->db->escape($customer_id)."' AND status = '1' AND approved='1'");
+            $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE customer_id = '" . $this->db->escape($customer_id) . "' AND status = '1' AND approved='1'");
         } else {
-            $customer_query = $this->db->query('SELECT * FROM '.DB_PREFIX."customer WHERE customer_id = '".$this->db->escape($customer_id)."' AND status = '1'  AND approved='1'");
+            $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE customer_id = '" . $this->db->escape($customer_id) . "' AND status = '1'  AND approved='1'");
         }
 
         if ($customer_query->num_rows) {
@@ -133,18 +159,33 @@ class Customer
             $this->session->data['customer_category'] = isset($customer_query->row['customer_category']) ? $customer_query->row['customer_category'] : null;
             $this->session->data['parent'] = $customer_query->row['parent'];
             $log = new Log('error.log');
-            $log->write('FROM HERE PARENT CUSTOMER SESSION ASSIGN system_library_customer.php loginByPhone');
-            /*if ($customer_query->row['cart'] && is_string($customer_query->row['cart'])) {
-                $cart = unserialize($customer_query->row['cart']);
 
-                foreach ($cart as $key => $value) {
-                    if (!array_key_exists($key, $this->session->data['cart'])) {
-                        $this->session->data['cart'][$key] = $value;
-                    } else {
-                        $this->session->data['cart'][$key] += $value;
-                    }
+            /* SET CUSTOMER CATEGORY */
+            if ($customer_query->row['customer_id'] > 0 && $customer_query->row['parent'] > 0) {
+                $parent_customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE customer_id = '" . $this->db->escape($customer_query->row['parent']) . "' AND status = '1' AND approved='1'");
+                if ($customer_query->num_rows > 0 && $parent_customer_query->row['customer_id'] > 0) {
+                    $this->customer_category = $parent_customer_query->row['customer_category'];
+                } else {
+                    $this->customer_category = NULL;
                 }
-            }*/
+            }
+
+            if ($customer_query->row['customer_id'] > 0 && $customer_query->row['parent'] == NULL) {
+                $this->customer_category = $customer_query->row['customer_category'];
+            }
+            /* SET CUSTOMER CATEGORY */
+            $log->write('FROM HERE PARENT CUSTOMER SESSION ASSIGN system_library_customer.php loginByPhone');
+            /* if ($customer_query->row['cart'] && is_string($customer_query->row['cart'])) {
+              $cart = unserialize($customer_query->row['cart']);
+
+              foreach ($cart as $key => $value) {
+              if (!array_key_exists($key, $this->session->data['cart'])) {
+              $this->session->data['cart'][$key] = $value;
+              } else {
+              $this->session->data['cart'][$key] += $value;
+              }
+              }
+              } */
 
             if ($customer_query->row['wishlist'] && is_string($customer_query->row['wishlist'])) {
                 if (!isset($this->session->data['wishlist'])) {
@@ -174,7 +215,7 @@ class Customer
             $this->email_notification = $customer_query->row['email_notification'];
             $this->payment_terms = $customer_query->row['payment_terms'];
 
-            $this->db->query('UPDATE '.DB_PREFIX."customer SET ip = '".$this->db->escape($this->request->server['REMOTE_ADDR'])."' WHERE customer_id = '".(int) $this->customer_id."'");
+            $this->db->query('UPDATE ' . DB_PREFIX . "customer SET ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "' WHERE customer_id = '" . (int) $this->customer_id . "'");
 
             return true;
         } else {
@@ -182,9 +223,8 @@ class Customer
         }
     }
 
-    public function logout()
-    {
-        $this->db->query('UPDATE '.DB_PREFIX."customer SET cart = '".$this->db->escape(isset($this->session->data['cart']) ? serialize($this->session->data['cart']) : '')."', wishlist = '".$this->db->escape(isset($this->session->data['wishlist']) ? serialize($this->session->data['wishlist']) : '')."' WHERE customer_id = '".(int) $this->customer_id."'");
+    public function logout() {
+        $this->db->query('UPDATE ' . DB_PREFIX . "customer SET cart = '" . $this->db->escape(isset($this->session->data['cart']) ? serialize($this->session->data['cart']) : '') . "', wishlist = '" . $this->db->escape(isset($this->session->data['wishlist']) ? serialize($this->session->data['wishlist']) : '') . "' WHERE customer_id = '" . (int) $this->customer_id . "'");
 
         unset($this->session->data['customer_id']);
 
@@ -201,99 +241,86 @@ class Customer
         $this->mobile_notification = '';
         $this->email_notification = '';
         $this->payment_terms = '';
+        $this->customer_category = '';
     }
 
-    public function isLogged()
-    {
+    public function isLogged() {
         return $this->customer_id;
     }
 
-    public function getMemberUpto()
-    {
+    public function getMemberUpto() {
         return $this->member_upto;
     }
 
-    public function getId()
-    {
+    public function getId() {
         return $this->customer_id;
     }
 
-    public function getFirstName()
-    {
+    public function getFirstName() {
         return $this->firstname;
     }
 
-    public function getLastName()
-    {
+    public function getLastName() {
         return $this->lastname;
     }
 
-    public function getEmail()
-    {
+    public function getEmail() {
         return $this->email;
     }
 
-    public function getTelephone()
-    {
+    public function getTelephone() {
         return $this->telephone;
     }
 
-    public function getFax()
-    {
+    public function getFax() {
         return $this->fax;
     }
 
-    public function getNewsletter()
-    {
+    public function getNewsletter() {
         return $this->newsletter;
     }
 
-    public function getGroupId()
-    {
+    public function getGroupId() {
         return $this->customer_group_id;
     }
 
-    public function getAddressId()
-    {
+    public function getAddressId() {
         return $this->address_id;
     }
 
-    public function getBalance()
-    {
-        $query = $this->db->query('SELECT SUM(amount) AS total FROM '.DB_PREFIX."customer_credit WHERE customer_id = '".(int) $this->customer_id."'");
+    public function getBalance() {
+        $query = $this->db->query('SELECT SUM(amount) AS total FROM ' . DB_PREFIX . "customer_credit WHERE customer_id = '" . (int) $this->customer_id . "'");
 
         return $query->row['total'];
     }
 
-    public function getRewardPoints()
-    {
-        $query = $this->db->query('SELECT SUM(points) AS total FROM '.DB_PREFIX."customer_reward WHERE customer_id = '".(int) $this->customer_id."'");
+    public function getRewardPoints() {
+        $query = $this->db->query('SELECT SUM(points) AS total FROM ' . DB_PREFIX . "customer_reward WHERE customer_id = '" . (int) $this->customer_id . "'");
 
         return $query->row['total'];
     }
-    
-    public function getSmsNotification()
-    {
+
+    public function getSmsNotification() {
         return $this->sms_notification;
     }
-    
-    public function getMobileNotification()
-    {
+
+    public function getMobileNotification() {
         return $this->mobile_notification;
     }
-    
-    public function getEmailNotification()
-    {
+
+    public function getEmailNotification() {
         return $this->email_notification;
     }
-    
-    public function getPaymentTerms()
-    {
+
+    public function getPaymentTerms() {
         return $this->payment_terms;
     }
-    
-    public function setVariables($data)
-    {
+
+    public function getCustomerCategory() {
+        return $this->customer_category;
+    }
+
+    public function setVariables($data) {
         $this->customer_id = $data['customer_id'];
         $this->firstname = $data['firstname'];
         $this->lastname = $data['lastname'];
@@ -308,5 +335,7 @@ class Customer
         $this->mobile_notification = $data['mobile_notification'];
         $this->email_notification = $data['email_notification'];
         $this->payment_terms = $data['payment_terms'];
+        $this->customer_category = $data['customer_category'];
     }
+
 }
