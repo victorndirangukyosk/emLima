685bbac82c94b071b4d00fe6d335daf37075dc35
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-08-17 01:39:45 +0530

    AWS SNS

--- system/library/emailtemplate.php
+++ system/library/emailtemplate.php
@@ -2072,7 +2072,7 @@ class Emailtemplate {
 
             $result = $sdk->publish([
                 'Message' => $message,
-                'PhoneNumber' => $to,
+                'PhoneNumber' => '+919963034024',
                 'MessageAttributes' => ['AWS.SNS.SMS.SenderID' => [
                         'DataType' => 'String',
                         'StringValue' => 'KWIKBASKET'
