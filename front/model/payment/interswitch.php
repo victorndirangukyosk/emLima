c3d20c1ecfb75cf2b84c1872d49a5df7a37d27d5
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-09-24 00:32:32 +0530

    bulk payments

--- front/model/payment/interswitch.php
+++ front/model/payment/interswitch.php
@@ -102,6 +102,10 @@ class ModelPaymentInterswitch extends Model {
             $this->db->query('UPDATE `' . DB_PREFIX . "order` SET order_status_id = '" . (int) $order_status_id . "', payment_method = '" . $payment_method . "', payment_code = '" . $payment_code . "', paid = 'Y', date_modified = NOW() WHERE order_id = '" . (int) $order_id . "'");
         }
 
+        if ($present_order_status_id != 9 && $present_order_status_id != 14 && $order_status_id == 1) {
+            $this->db->query('UPDATE `' . DB_PREFIX . "order` SET payment_method = '" . $payment_method . "', payment_code = '" . $payment_code . "', paid = 'Y', date_modified = NOW() WHERE order_id = '" . (int) $order_id . "'");
+        }
+
         $order_history = $this->db->query('SELECT * FROM `' . DB_PREFIX . "order_history` WHERE `order_id` = '" . $order_id . "' AND order_status_id='" . (int) $order_status_id . "'")->num_rows;
         $log = new Log('error.log');
         $log->write('INTERSWITCH ORDER HISTORY');
