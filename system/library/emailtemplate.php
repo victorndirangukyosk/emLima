7e305642acaea6907328f6dfc62bf2a1da44a0cc
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-10-22 16:49:16 +0530

    FCM API KEY CHANGED

--- system/library/emailtemplate.php
+++ system/library/emailtemplate.php
@@ -2287,8 +2287,9 @@ class Emailtemplate {
                 if (isset($deviceId) && isset($to)) {
                     $log->write('api key');
 
-                    $apiKey = $this->config->get('config_seller_api_key');
+                    //$apiKey = $this->config->get('config_seller_api_key');
                     //$apiKey = 'AIzaSyAK3VgDt_MTGRaTMfs_9v_YdsK6tjFRsoo';
+                    $apiKey = 'AIzaSyB1oIypAqDhYW7iECyxxIY9BgmtGdOtQPo';
 
                     $log->write($apiKey);
                     $client = new FCMClient();
