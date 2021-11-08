65a5c8ff65c03386b1610b7978f885ae78eb1c54
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-11-08 14:48:03 +0530

    pezesha user registration

--- admin/controller/pezesha/pezesha.php
+++ admin/controller/pezesha/pezesha.php
@@ -33,4 +33,29 @@ class ControllerPezeshaPezesha extends Controller {
         $json = $result;
     }
 
+    public function userregistration() {
+
+        $log = new Log('error.log');
+        $body = array('grant_type' => 'client_credentials', 'provider' => 'users', 'client_secret' => $this->config->get('pezesha_client_secret'), 'client_id' => $this->config->get('pezesha_client_id'), 'merchant _key' => $this->config->get('pezesha_merchant_key'));
+        $curl = curl_init();
+        if (ENV == 'production') {
+            curl_setopt($curl, CURLOPT_URL, 'https://staging.api.pezesha.com/mfi/v1/borrowers');
+            curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type:application/json', 'Authentication:Bearer ']);
+        } else {
+            curl_setopt($curl, CURLOPT_URL, 'https://staging.api.pezesha.com/mfi/v1/borrowers');
+            curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type:application/json', 'Authentication:Bearer ']);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $body); //Setting post data as xml
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        $result = curl_exec($curl);
+
+        $log->write($result);
+        curl_close($curl);
+        $result = json_decode($result, true);
+        $json = $result;
+    }
+
 }
