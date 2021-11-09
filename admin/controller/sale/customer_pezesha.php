8ae9abe719816cdd7f5746ddccbe15974488c7d5
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-11-09 16:27:13 +0530

    customer data ingestion to pezesha

--- admin/controller/sale/customer_pezesha.php
+++ admin/controller/sale/customer_pezesha.php
@@ -35,7 +35,7 @@ class ControllerSaleCustomerPezesha extends Controller {
         $this->response->addHeader('Content-Type: application/json');
         $this->response->setOutput(json_encode($response));
     }
-    
+
     public function optout() {
 
         $customer_id = $this->request->post['customer_id'];
@@ -46,6 +46,22 @@ class ControllerSaleCustomerPezesha extends Controller {
         $response = NULL;
         if (is_array($customer_device_info) && count($customer_device_info) > 0) {
             $response = $this->load->controller('pezesha/pezesha/optout', $customer_device_info['customer_id']);
+        }
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($response));
+    }
+
+    public function dataingestion() {
+
+        $customer_id = $this->request->post['customer_id'];
+        $this->load->model('sale/customer');
+        $log = new Log('error.log');
+        $log->write($customer_id);
+        $customer_device_info = $this->model_sale_customer->getCustomer($customer_id);
+        $response = NULL;
+        if (is_array($customer_device_info) && count($customer_device_info) > 0) {
+            $response = $this->load->controller('pezesha/pezesha/dataingestion', $customer_device_info['customer_id']);
         }
 
         $this->response->addHeader('Content-Type: application/json');
