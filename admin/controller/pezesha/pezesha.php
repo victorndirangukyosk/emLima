7405b49cbf0c9155572ca2a9dd04383d82d6d564
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-11-09 16:53:07 +0530

    pezesha

--- admin/controller/pezesha/pezesha.php
+++ admin/controller/pezesha/pezesha.php
@@ -196,10 +196,11 @@ class ControllerPezeshaPezesha extends Controller {
 
         foreach ($customer_order_info as $order_info) {
             $order_transaction_info = $this->model_sale_order->getOrderTransactionId($order_info['order_id']);
-            $transactions['transaction_id'] = $order_transaction_info['transaction_id'];
-            $transactions['merchant_id'] = $order_info['customer_id'];
-            $transactions['face_amount'] = $order_info['total'];
-            $transactions['transaction_time'] = $order_info['date_added'];
+            $transactions[]['transaction_id'] = $order_transaction_info['transaction_id'];
+            $transactions[]['merchant_id'] = $customer_id;
+            $transactions[]['face_amount'] = $order_info['total'];
+            $transactions[]['transaction_time'] = $order_info['date_added'];
+            $transactions[]['other_details'] = array('key' => 'location', 'value' => $order_info['shipping_address'], 'key' => 'category', 'value' => 'Fresh Produce');
         }
         $log->write($transactions);
         exit;
