aeead99fe8b585b7ea0e6c6e3ad256a2be4054e5
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-11-11 20:49:08 +0530

    pezesha loan offers fetch

--- front/controller/payment/pezesha.php
+++ front/controller/payment/pezesha.php
@@ -93,10 +93,10 @@ class ControllerPaymentPezesha extends Controller {
         $log->write($result);
         $json = $result;
 
-        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/account/loan_offers.tpl', $json)) {
-            $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/account/loan_offers.tpl', $json));
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/account/loan_offers.tpl')) {
+            $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/account/loan_offers.tpl', $result));
         } else {
-            $this->response->setOutput($this->load->view('default/template/account/loan_offers.tpl', $json));
+            $this->response->setOutput($this->load->view('default/template/account/loan_offers.tpl', $result));
         }
     }
 
