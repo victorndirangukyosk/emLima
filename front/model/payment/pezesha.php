939c971601a205ec04d5df685e2d14e74ff4a6d0
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-11-11 21:17:39 +0530

    pezesha limit

--- front/model/payment/pezesha.php
+++ front/model/payment/pezesha.php
@@ -1,9 +1,8 @@
 <?php
 
-class ModelPaymentPezesha extends Model
-{
-    public function getMethod($total)
-    {
+class ModelPaymentPezesha extends Model {
+
+    public function getMethod($total) {
         $this->load->language('payment/pezesha');
 
         if ($this->config->get('pezesha_total') > 0 && $this->config->get('pezesha_total') > $total) {
@@ -15,10 +14,9 @@ class ModelPaymentPezesha extends Model
         $log = new Log('error.log');
         $log->write('pyment method checking');
         $log->write($total);
-        
+
         //in case customer experience team apply coupon , then cart total will be 0
-         if($_SESSION["ce_id"] > 0  )
-        {
+        if ($_SESSION["ce_id"] > 0) {
             $status = true;
         }
         $log->write($status);
@@ -31,6 +29,7 @@ class ModelPaymentPezesha extends Model
                 'code' => 'pezesha',
                 'title' => $this->language->get('text_title'),
                 'terms' => $this->language->get('text_terms'),
+                'terms1' => 'Available Pezesha Amount - ' . $this->session->data['pezesha_amount_limit'],
                 'sort_order' => $this->config->get('pezesha_sort_order'),
             ];
         }
@@ -38,4 +37,5 @@ class ModelPaymentPezesha extends Model
         $log->write($method_data);
         return $method_data;
     }
+
 }
