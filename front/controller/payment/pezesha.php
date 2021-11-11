b6f9043a46aa555d2a9e066baac5a084350687bc
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-11-11 20:21:06 +0530

    pezesha fetch loan offers

--- front/controller/payment/pezesha.php
+++ front/controller/payment/pezesha.php
@@ -92,24 +92,12 @@ class ControllerPaymentPezesha extends Controller {
         $result = json_decode($result, true);
         $log->write($result);
         $json = $result;
-        $html = "<div class='col-md-12' id='pay_with'></div>";
-        if ($result['data']['status'] == 200 && $result['data']['response_code'] == 0 && $result['data']['error'] == false) {
-            $html = "<div class='col-md-12' id='pay_with'>" . $result['message'] . "</div>";
-        }
-        if ($result['data']['status'] == 200 && $result['data']['response_code'] == 0 && $result['data']['error'] == true) {
-            $html = "<div class='col-md-12' id='pay_with'>" . $result['message'] . "</div>";
-        }
-        $log->write('html');
-        $log->write($html);
-        $log->write('html');
-        return $html;
-        //return $json;
 
-        /* $json['status'] = true;
-          $json['data'] = $result;
-
-          $this->response->addHeader('Content-Type: application/json');
-          $this->response->setOutput(json_encode($json)); */
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/account/loan_offers.tpl')) {
+            $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/account/loan_offers.tpl', $json));
+        } else {
+            $this->response->setOutput($this->load->view('default/template/account/loan_offers.tpl', $json));
+        }
     }
 
     public function applyloan() {
