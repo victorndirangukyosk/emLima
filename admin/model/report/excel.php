890c834c6f701f3f4f545201a0c7448dfb0690fc
Sri Divya <sridivya.talluri@technobraingroup.com>
2021-11-22 17:04:07 +0530

    fix

--- admin/model/report/excel.php
+++ admin/model/report/excel.php
@@ -9088,7 +9088,8 @@ class ModelReportExcel extends Model {
                 $data['totals'][] = [
                     'title' => $total['title'],
                     'code' => $total['code'],
-                    'text' => $this->currency->format($total['value'], $order_info['currency_code'], $order_info['currency_value']),
+                    // 'text' => $this->currency->format($total['value'], $order_info['currency_code'], $order_info['currency_value']),
+                    'text' => $this->currency->format($total['value']),
                 ];
 
                 if ('total' == $total['code']) {
