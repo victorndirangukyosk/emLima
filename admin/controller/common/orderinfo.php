051072c9c0d3bf608093ee1fab2cfb3cac4e706f
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-11-26 15:27:14 +0530

    order info

--- admin/controller/common/orderinfo.php
+++ admin/controller/common/orderinfo.php
@@ -260,9 +260,9 @@ class ControllerCommonOrderinfo extends Controller {
             }
 
             if ($this->user->isLogged() && $this->session->data['token'] != NULL) {
-                $data['order_link'] = $this->url->adminLink('sale/order', 'token=' . $this->session->data['token']);
+                $data['order_link'] = $this->url->adminLinks('sale/order', 'token=' . $this->session->data['token']);
             } else {
-                $data['login_link'] = $this->url->adminLink('common/login');
+                $data['login_link'] = $this->url->adminLinks('common/login');
             }
 
 
