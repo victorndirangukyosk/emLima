290f353f5c329dc0de80d8e7ac8d331b624458ad
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-11-29 16:39:49 +0530

    api changes

--- front/controller/api/customer/pezesha.php
+++ front/controller/api/customer/pezesha.php
@@ -6,6 +6,7 @@ class ControllerApiCustomerPezesha extends Controller {
         $data['orders'] = NULL;
         $this->load->model('account/order');
         $pezesha_loans = $this->model_account_order->getPezeshaloans();
+        $data['message'] = count($pezesha_loans) > 0 ? 'Pezesha Loans Fetched Successfully!' : 'Pezesha Loans Not Found!';
         foreach ($pezesha_loans as $pezesha_loan) {
             $pezesha_loan['total'] = $this->currency->format($pezesha_loan['total']);
             $data['orders'][] = $pezesha_loan;
