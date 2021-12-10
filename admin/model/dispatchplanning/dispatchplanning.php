99f17fa88dfb349c9960877333ceaadc8d740d42
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-12-10 14:50:35 +0530

    dispatch planning

--- admin/model/dispatchplanning/dispatchplanning.php
+++ admin/model/dispatchplanning/dispatchplanning.php
@@ -8,4 +8,19 @@ class ModelDispatchplanningDispatchplanning extends Model {
         return $dispatch_id;
     }
 
+    public function CheckVehicleAssigned($vehicle_id, $delivery_date, $delivery_timeslot) {
+        $query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "dispatch_assignment WHERE vehicle_id = '" . (int) $vehicle_id . "' AND delivery_date = '" . $delivery_date . "' AND delivery_time_slot = '" . $delivery_timeslot . "'");
+        return $query->row;
+    }
+
+    public function CheckDriverAssigned($driver_id, $delivery_date, $delivery_timeslot) {
+        $query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "dispatch_assignment WHERE driver_id = '" . (int) $driver_id . "' AND delivery_date = '" . $delivery_date . "' AND delivery_time_slot = '" . $delivery_timeslot . "'");
+        return $query->row;
+    }
+
+    public function CheckDeliveryExecutiveAssigned($delivery_executive, $delivery_date, $delivery_timeslot) {
+        $query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "dispatch_assignment WHERE delivery_executive_id = '" . (int) $delivery_executive . "' AND delivery_date = '" . $delivery_date . "' AND delivery_time_slot = '" . $delivery_timeslot . "'");
+        return $query->row;
+    }
+
 }
