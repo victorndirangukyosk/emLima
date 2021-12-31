ab967690624eab55fb866c8db2dc42592faf6634
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-12-31 16:02:40 +0530

    schedulers

--- front/controller/pdf/pdf.php
+++ front/controller/pdf/pdf.php
@@ -3,9 +3,11 @@
 class ControllerPdfPdf extends Controller {
 
     public function index($data) {
+        $data = NULL;
         try {
             $this->load->model('sale/order');
             $rows = $this->model_sale_order->getOrders($data);
+            $data['orders'] = $rows;
             require_once DIR_ROOT . '/vendor/autoload.php';
             $pdf = new \mikehaertl\wkhtmlto\Pdf;
             $template = $this->load->view($this->config->get('config_template') . '/template/pdf/pdf.tpl', $rows);
