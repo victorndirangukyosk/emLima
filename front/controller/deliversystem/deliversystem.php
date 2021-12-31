8e542b7a97f68be3297704cd6ba8261e87c50547
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-12-31 17:41:13 +0530

    more filters

--- front/controller/deliversystem/deliversystem.php
+++ front/controller/deliversystem/deliversystem.php
@@ -1882,26 +1882,31 @@ class ControllerDeliversystemDeliversystem extends Controller {
         $all_customers = $this->model_account_order->getUnpaidOrdersCutomerIds();
         $all_customers = array_column($all_customers, 'customer_id');
         $all_customers = array_unique($all_customers);
+        $all_customers = implode(",", $all_customers);
+        $data['filter_customer_id_array'] = $all_customers;
+        $all_unpaid_customers = $this->model_sale_order->getCustomersNew($data);
         $log = new Log('error.log');
         $data['pending_order_id'] = NULL;
 
-        foreach ($all_customers as $customer) {
+        foreach ($all_unpaid_customers as $customer) {
+            $data['customer_id'] = $customer['customer_id'];
             $log->write($customer);
             $page = 1;
-            $results_orders = $this->model_account_order->getOrdersNewByCustomerId($customer, ($page - 1) * 10, 10, $NoLimit = true);
+            $results_orders = $this->model_account_order->getOrdersNewByCustomerId($customer['customer_id'], ($page - 1) * 10, 10, $NoLimit = true);
             $PaymentFilter = ['mPesa On Delivery', 'Cash On Delivery', 'mPesa Online', 'Corporate Account/ Cheque Payment', 'PesaPal', 'Interswitch', 'Pezesha'];
             if (count($results_orders) > 0) {
                 foreach ($results_orders as $order) {
                     if (in_array($order['payment_method'], $PaymentFilter) && ($order['order_status_id'] == 4 || $order['order_status_id'] == 5)) {
                         $order['transcation_id'] = $this->model_sale_order->getOrderTransactionId($order['order_id']);
                         if (empty($order['transcation_id'])) {
-                            $data['pending_order_id'][$customer][] = $order['order_id'];
+                            $data['pending_order_id'][$customer['customer_id']][] = $order['order_id'];
                         }
                     }
                 }
             }
-            $data['filter_order_id_array'] = implode(",", $data['pending_order_id'][$customer]);
-            $this->model_report_excel->download_customer_unpaid_order_excel($data);
+            $data['filter_order_id_array'] = implode(",", $data['pending_order_id'][$customer['customer_id']]);
+            $this->load->controller('pdf/pdf', $data);
+            //$this->model_report_excel->download_customer_unpaid_order_excel($data);
         }
         $json['status'] = 200;
         $json['data'] = $data['pending_order_id'];
@@ -1923,11 +1928,15 @@ class ControllerDeliversystemDeliversystem extends Controller {
         $all_customers = $this->model_account_order->getUnpaidOrdersCutomerIds();
         $all_customers = array_column($all_customers, 'customer_id');
         $all_customers = array_unique($all_customers);
+        $all_customers = implode(",", $all_customers);
+        $data['filter_customer_id_array'] = $all_customers;
+        $all_unpaid_customers = $this->model_sale_order->getCustomersNew($data);
         $data['pending_order_id'] = NULL;
 
-        foreach ($all_customers as $customer) {
+        foreach ($all_unpaid_customers as $customer) {
+            $data['customer_id'] = $customer['customer_id'];
             $page = 1;
-            $results_orders = $this->model_account_order->getOrdersNewByCustomerId($customer, ($page - 1) * 10, 10, $NoLimit = true);
+            $results_orders = $this->model_account_order->getOrdersNewByCustomerId($customer['customer_id'], ($page - 1) * 10, 10, $NoLimit = true);
             $PaymentFilter = ['mPesa On Delivery', 'Cash On Delivery', 'mPesa Online', 'Corporate Account/ Cheque Payment', 'PesaPal', 'Interswitch', 'Pezesha'];
             if (count($results_orders) > 0) {
                 foreach ($results_orders as $order) {
@@ -1939,8 +1948,9 @@ class ControllerDeliversystemDeliversystem extends Controller {
                     }
                 }
             }
-            $data['filter_order_id_array'] = implode(",", $data['pending_order_id'][$customer]);
-            $this->model_report_excel->download_customer_unpaid_order_excel($data);
+            $data['filter_order_id_array'] = implode(",", $data['pending_order_id'][$customer['customer_id']]);
+            $this->load->controller('pdf/pdf', $data);
+            //$this->model_report_excel->download_customer_unpaid_order_excel($data);
         }
         $json['status'] = 200;
         $json['data'] = $data['pending_order_id'];
