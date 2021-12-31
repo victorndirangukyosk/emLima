a9f4192a5e2045fdd6922014795fc2724e948589
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2021-12-31 16:30:17 +0530

    pdf library

--- front/controller/pdf/pdf.php
+++ front/controller/pdf/pdf.php
@@ -3,14 +3,16 @@
 class ControllerPdfPdf extends Controller {
 
     public function index($data) {
-        $data = NULL;
+        $dat = NULL;
         try {
             $this->load->model('sale/order');
             $rows = $this->model_sale_order->getOrders($data);
-            $data['orders'] = $rows;
+            $log = new Log('error.log');
+            $log->write($rows);
+            $dat['orders'] = $rows;
             require_once DIR_ROOT . '/vendor/autoload.php';
             $pdf = new \mikehaertl\wkhtmlto\Pdf;
-            $template = $this->load->view($this->config->get('config_template') . '/template/pdf/pdf.tpl', $data);
+            $template = $this->load->view($this->config->get('config_template') . '/template/pdf/pdf.tpl', $dat);
             $pdf->addPage($template);
             if (!$pdf->send("KwikBasket_UnPaid_Orders" . $data['customer_id'] . ".pdf")) {
                 $error = $pdf->getError();
