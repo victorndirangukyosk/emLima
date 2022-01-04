9b6a10e75c371b35e2f722e00f3581c86fb24e80
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-01-04 12:40:17 +0530

    missed rejected product

--- front/controller/api/customer/order.php
+++ front/controller/api/customer/order.php
@@ -4223,6 +4223,10 @@ class ControllerApiCustomerOrder extends Controller {
             $this->error['order_id'] = 'Order Id Is Invalid!';
         }
 
+        if (empty($args['issue_type']) || ($args['issue_type'] != 'missed' && $args['issue_type'] != 'rejected')) {
+            $this->error['order_id'] = 'Please Select Product Missed Or Rejected!';
+        }
+
         return !$this->error;
     }
 
