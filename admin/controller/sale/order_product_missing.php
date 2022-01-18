cb66df8222a7ee7dbbfd2cffd4cab188955c1c36
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-01-18 19:45:00 +0530

    generate invoice

--- admin/controller/sale/order_product_missing.php
+++ admin/controller/sale/order_product_missing.php
@@ -1166,4 +1166,171 @@ class ControllerSaleOrderProductMissing extends Controller {
         }
     }
 
+    public function translateAmountToWords(int $number) {
+        /*
+         * A recursive function to turn digits into words
+         * Numbers must be integers from -999,999,999,999 to 999,999,999,999 inclusive.
+         * Zero is a special case, it cause problems even with typecasting if we don't deal with it here
+         */
+        $max_size = pow(10, 18);
+        if (!$number) {
+            return 'zero';
+        }
+        if ($number < abs($max_size)) {
+            switch ($number) {
+                // set up some rules for converting digits to words
+                case $number < 0:
+                    $prefix = 'negative';
+                    $suffix = $this->translateAmountToWords(-1 * $number);
+                    $string = $prefix . ' ' . $suffix;
+                    break;
+                case 1:
+                    $string = 'one';
+                    break;
+                case 2:
+                    $string = 'two';
+                    break;
+                case 3:
+                    $string = 'three';
+                    break;
+                case 4:
+                    $string = 'four';
+                    break;
+                case 5:
+                    $string = 'five';
+                    break;
+                case 6:
+                    $string = 'six';
+                    break;
+                case 7:
+                    $string = 'seven';
+                    break;
+                case 8:
+                    $string = 'eight';
+                    break;
+                case 9:
+                    $string = 'nine';
+                    break;
+                case 10:
+                    $string = 'ten';
+                    break;
+                case 11:
+                    $string = 'eleven';
+                    break;
+                case 12:
+                    $string = 'twelve';
+                    break;
+                case 13:
+                    $string = 'thirteen';
+                    break;
+                // fourteen handled later
+                case 15:
+                    $string = 'fifteen';
+                    break;
+                case $number < 20:
+                    $string = $this->translateAmountToWords($number % 10);
+                    // eighteen only has one "t"
+                    if (18 == $number) {
+                        $suffix = 'een';
+                    } else {
+                        $suffix = 'teen';
+                    }
+                    $string .= $suffix;
+                    break;
+                case 20:
+                    $string = 'twenty';
+                    break;
+                case 30:
+                    $string = 'thirty';
+                    break;
+                case 40:
+                    $string = 'forty';
+                    break;
+                case 50:
+                    $string = 'fifty';
+                    break;
+                case 60:
+                    $string = 'sixty';
+                    break;
+                case 70:
+                    $string = 'seventy';
+                    break;
+                case 80:
+                    $string = 'eighty';
+                    break;
+                case 90:
+                    $string = 'ninety';
+                    break;
+                case $number < 100:
+                    $prefix = $this->translateAmountToWords($number - $number % 10);
+                    $suffix = $this->translateAmountToWords($number % 10);
+                    $string = $prefix . ' ' . $suffix;
+                    break;
+                // handles all number 100 to 999
+                case $number < pow(10, 3):
+                    // floor return a float not an integer
+                    $prefix = $this->translateAmountToWords(intval(floor($number / pow(10, 2)))) . ' hundred';
+                    if ($number % pow(10, 2)) {
+                        $suffix = ' and ' . $this->translateAmountToWords($number % pow(10, 2));
+                    }
+                    $string = $prefix . $suffix;
+                    break;
+                case $number < pow(10, 6):
+                    // floor return a float not an integer
+                    $prefix = $this->translateAmountToWords(intval(floor($number / pow(10, 3)))) . ' thousand';
+                    if ($number % pow(10, 3)) {
+                        $suffix = $this->translateAmountToWords($number % pow(10, 3));
+                    }
+                    $string = $prefix . ' ' . $suffix;
+                    break;
+                case $number < pow(10, 9):
+                    // floor return a float not an integer
+                    $prefix = $this->translateAmountToWords(intval(floor($number / pow(10, 6)))) . ' million';
+                    if ($number % pow(10, 6)) {
+                        $suffix = $this->translateAmountToWords($number % pow(10, 6));
+                    }
+                    $string = $prefix . ' ' . $suffix;
+                    break;
+                case $number < pow(10, 12):
+                    // floor return a float not an integer
+                    $prefix = $this->translateAmountToWords(intval(floor($number / pow(10, 9)))) . ' billion';
+                    if ($number % pow(10, 9)) {
+                        $suffix = $this->translateAmountToWords($number % pow(10, 9));
+                    }
+                    $string = $prefix . ' ' . $suffix;
+                    break;
+                case $number < pow(10, 15):
+                    // floor return a float not an integer
+                    $prefix = $this->translateAmountToWords(intval(floor($number / pow(10, 12)))) . ' trillion';
+                    if ($number % pow(10, 12)) {
+                        $suffix = $this->translateAmountToWords($number % pow(10, 12));
+                    }
+                    $string = $prefix . ' ' . $suffix;
+                    break;
+                // Be careful not to pass default formatted numbers in the quadrillions+ into this function
+                // Default formatting is float and causes errors
+                case $number < pow(10, 18):
+                    // floor return a float not an integer
+                    $prefix = $this->translateAmountToWords(intval(floor($number / pow(10, 15)))) . ' quadrillion';
+                    if ($number % pow(10, 15)) {
+                        $suffix = $this->translateAmountToWords($number % pow(10, 15));
+                    }
+                    $string = $prefix . ' ' . $suffix;
+                    break;
+            }
+        } else {
+            echo "ERROR with - $number<br/> Number must be an integer between -" . number_format($max_size, 0, '.', ',') . ' and ' . number_format($max_size, 0, '.', ',') . ' exclusive.';
+        }
+
+        return $string;
+    }
+
+    public function getUser($id) {
+        if ($id) {
+            $query = $this->db->query('SELECT * FROM `' . DB_PREFIX . "customer`  WHERE customer_id ='" . $id . "'");
+
+            return $query->row['fax'];
+        }
+    }
+
 }
