923e3e2aa4dd9650372230eed15d4767fd0f9030
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-01-23 23:52:17 +0530

    missing products

--- front/model/checkout/order.php
+++ front/model/checkout/order.php
@@ -2267,4 +2267,9 @@ class ModelCheckoutOrder extends Model {
         $this->db->query('UPDATE `' . DB_PREFIX . 'missing_products` SET new_order_id ="' . $new_order_id . '", updated_at = NOW() WHERE order_id="' . $order_id . '"');
     }
 
+    public function getNewOrderIdByMissingProductOrderId($order_id) {
+        $new_order_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "missing_products` WHERE order_id = '" . (int) $order_id . "' AND new_order_id > 0");
+        return $new_order_query->row;
+    }
+
 }
