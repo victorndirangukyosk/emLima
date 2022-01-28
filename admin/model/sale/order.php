6ea0484db244e681a0565059eb697cc8a75dc4b8
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-01-28 15:18:01 +0530

    Update order.php

--- admin/model/sale/order.php
+++ admin/model/sale/order.php
@@ -5319,11 +5319,11 @@ class ModelSaleOrder extends Model {
        
 
         if (isset($data['filter_status']) && !is_null($data['filter_status'])) {
-            $implode[] = "c.status = '" . (int) $data['filter_status'] . "'";
+            $sql .= " and c.status = '" . (int) $data['filter_status'] . "'";
         }
 
         if (isset($data['filter_payment_terms']) && !is_null($data['filter_payment_terms'])) {
-            $implode[] = "c.payment_terms = '" . $data['filter_payment_terms'] . "'";
+            $sql .= " and c.payment_terms = '" . $data['filter_payment_terms'] . "'";
         }
 
 
