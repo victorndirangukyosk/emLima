d808313407838d6cd6de8d0ad5a14dcbc40067f9
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-02-04 10:52:16 +0530

    hybrid payment

--- front/model/payment/wallet.php
+++ front/model/payment/wallet.php
@@ -65,4 +65,17 @@ class ModelPaymentWallet extends Model {
         $this->db->query('UPDATE ' . DB_PREFIX . "order SET paid='Y', amount_partialy_paid = 0   WHERE order_id='" . (int) $order_id . "'"); //,total='" . (float) $total . "'
     }
 
+    public function addTransactionCreditForHybridPayment($customer_id, $description = '', $amount = '', $order_id = 0) {
+
+
+        // $query = $this->db->query('SELECT total AS total FROM ' . DB_PREFIX . "order WHERE customer_id = '" . (int) $this->customer->getId() . "' AND order_id = '" . (int) $order_id . "'");
+        // $total = $query->row['total'];
+        // $description = 'Wallet amount deducted#' . $order_id;
+        // $this->db->query('DELETE FROM ' . DB_PREFIX . "customer_credit WHERE customer_id = '" . (int) $customer_id . "' and  order_id = '" . (int) $order_id . "'");
+        $this->db->query('INSERT INTO ' . DB_PREFIX . "customer_credit SET customer_id = '" . (int) $customer_id . "', order_id = '" . (int) $order_id . "', description = '" . $description . "', amount = '" . (float) ($amount * -1) . "', date_added = NOW()");
+        $this->db->query('INSERT INTO ' . DB_PREFIX . "order_transaction_id SET customer_id = '" . (int) $customer_id . "', order_id = '" . (int) $order_id . "', transaction_id = 'Paid from wallet amount (T)'");
+
+        $this->db->query('UPDATE ' . DB_PREFIX . "order SET paid='P', amount_partialy_paid = '" . $amount . "'   WHERE order_id='" . (int) $order_id . "'"); //,total='" . (float) $total . "'
+    }
+
 }
