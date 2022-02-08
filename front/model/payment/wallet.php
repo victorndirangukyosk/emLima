cd0b565720a5187caa693e445049add83fb0b500
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-02-08 20:10:56 +0530

    hybrid payment

--- front/model/payment/wallet.php
+++ front/model/payment/wallet.php
@@ -65,6 +65,11 @@ class ModelPaymentWallet extends Model {
         $this->db->query('UPDATE ' . DB_PREFIX . "order SET paid='Y', amount_partialy_paid = 0   WHERE order_id='" . (int) $order_id . "'"); //,total='" . (float) $total . "'
     }
 
+    public function getOrder($order_id) {
+        $query = $this->db->query('SELECT * FROM `' . DB_PREFIX . "order` WHERE order_id = '" . (int) $order_id . "'");
+        return $query->row;
+    }
+
     public function addTransactionCreditForHybridPayment($customer_id, $description = '', $amount = '', $order_id = 0, $paid, $amount_partialy_paid) {
 
 
@@ -74,8 +79,10 @@ class ModelPaymentWallet extends Model {
         // $this->db->query('DELETE FROM ' . DB_PREFIX . "customer_credit WHERE customer_id = '" . (int) $customer_id . "' and  order_id = '" . (int) $order_id . "'");
         $this->db->query('INSERT INTO ' . DB_PREFIX . "customer_credit SET customer_id = '" . (int) $customer_id . "', order_id = '" . (int) $order_id . "', description = '" . $description . "', amount = '" . (float) ($amount * -1) . "', date_added = NOW()");
         $this->db->query('INSERT INTO ' . DB_PREFIX . "order_transaction_id SET customer_id = '" . (int) $customer_id . "', order_id = '" . (int) $order_id . "', transaction_id = 'Paid from wallet amount (T)', created_at = NOW()");
-
-        $this->db->query('UPDATE ' . DB_PREFIX . "order SET paid='" . $paid . "', amount_partialy_paid = '" . $amount_partialy_paid . "'   WHERE order_id='" . (int) $order_id . "'"); //,total='" . (float) $total . "'
+        $order_details = $this->getOrder($order_id);
+        if ($order_details['paid'] == 'P' || $order_details['paid'] == 'N') {
+            $this->db->query('UPDATE ' . DB_PREFIX . "order SET paid='" . $paid . "', amount_partialy_paid = '" . $amount_partialy_paid . "'   WHERE order_id='" . (int) $order_id . "'"); //,total='" . (float) $total . "'
+        }
     }
 
 }
