5b86d74ea6f834f6d0121f1e74e77472d91cf090
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-02-14 12:28:06 +0530

    hybrid payment

--- front/controller/checkout/payment_method.php
+++ front/controller/checkout/payment_method.php
@@ -163,7 +163,7 @@ class ControllerCheckoutPaymentMethod extends Controller {
             }
         } if ($this->customer->getCustomerPezeshaId() != NULL && $this->customer->getCustomerPezeshauuId() != NULL && $this->config->get('pezesha_status')) {
             foreach ($data['payment_methods'] as $payment_method) {
-                if ($payment_method['code'] != 'pezesha' && $payment_method['code'] != 'wallet') {
+                if ($payment_method['code'] != 'pezesha') {
                     unset($data['payment_methods'][$payment_method['code']]);
                 }
             }
