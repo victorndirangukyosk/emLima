ad3fd017fb2a410d65bca0ac706cabea35e5c0ae
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-03-02 13:35:10 +0530

    Create customer_financial_statement.php

---
+++ admin/language/english/report/customer_financial_statement.php
@@ -0,0 +1,26 @@
+<?php
+
+// Heading
+$_['heading_title'] = 'Customer Financial Statement';
+
+// Text
+$_['text_list'] = 'List';
+$_['text_all_status'] = 'All Statuses';
+
+// Column
+$_['column_customer'] = 'Customer Name';
+$_['column_email'] = 'E-Mail';
+$_['column_customer_group'] = 'Customer Group';
+$_['column_status'] = 'Status';
+$_['column_orders'] = 'No. Orders';
+$_['column_order_id'] = 'Order Id';
+$_['column_products'] = 'No. Products';
+$_['column_total'] = 'Total';
+$_['column_sub_total'] = 'Sub Total';
+$_['column_action'] = 'Action';
+
+// Entry
+$_['entry_date_start'] = 'Date Start';
+$_['entry_date_end'] = 'Date End';
+$_['entry_status'] = 'Order Status';
+$_['entry_customer'] = 'Customer';
