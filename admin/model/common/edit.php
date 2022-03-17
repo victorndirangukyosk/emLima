e4aadddcffebda9547a6becb68a84e526333a391
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-03-17 11:29:56 +0530

    Update edit.php

--- admin/model/common/edit.php
+++ admin/model/common/edit.php
@@ -71,6 +71,7 @@ class ModelCommonEdit extends Model
 
     public function mychangeStatus($type, $ids, $status, $extension = false)
     {
+
         if ($extension) {
             foreach ($ids as $id) {
                 $this->db->query('UPDATE '.DB_PREFIX."setting SET `value` = {$status} WHERE `code` = '{$id}' AND `key` = '{$id}_status'", 'query');
@@ -78,6 +79,31 @@ class ModelCommonEdit extends Model
         } else {
             foreach ($ids as $id) {
                 $this->db->query('UPDATE '.DB_PREFIX."product_to_store SET status = {$status} WHERE product_store_id = {$id}", 'query');
+            
+                if($type=='product_to_store')
+                {
+       
+                   $log = new Log('error.log');
+                   $this->load->model('user/user_activity');
+       
+                   $activity_data = [
+                       'user_id' => $this->user->getId(),
+                       'name' => $this->user->getFirstName() . ' ' . $this->user->getLastName(),
+                       'user_group_id' => $this->user->getGroupId(),
+                       'product_store_id' => $id,
+                   ];
+                        //  $log->write('product status modified');
+       
+                   if($status==0)
+                   {
+                   $this->model_user_user_activity->addActivity('vendor_product_disabled', $activity_data);
+                   }
+                   else{
+                       $this->model_user_user_activity->addActivity('vendor_product_enabled', $activity_data);
+       
+                   }
+                   //  $log->write('product status modified');
+                }
             }
         }
     }
