d9259250e72068604f2cc34e2c77a0e8476e5fb4
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-03-17 09:57:19 +0530

    multiple records status change Activity log

--- admin/model/common/edit.php
+++ admin/model/common/edit.php
@@ -11,6 +11,56 @@ class ModelCommonEdit extends Model
         } else {
             foreach ($ids as $id) {
                 $this->db->query('UPDATE '.DB_PREFIX."{$type} SET status = {$status} WHERE {$type}_id = {$id}", 'query');
+            
+             // Add to activity log
+         if($type=='product')
+         {
+
+            $log = new Log('error.log');
+            $this->load->model('user/user_activity');
+
+            $activity_data = [
+                'user_id' => $this->user->getId(),
+                'name' => $this->user->getFirstName() . ' ' . $this->user->getLastName(),
+                'user_group_id' => $this->user->getGroupId(),
+                'product_id' => $id,
+            ];
+                 //  $log->write('product status modified');
+
+            if($status==0)
+            {
+            $this->model_user_user_activity->addActivity('product_disabled', $activity_data);
+            }
+            else{
+                $this->model_user_user_activity->addActivity('product_enabled', $activity_data);
+
+            }
+            //  $log->write('product status modified');
+         }
+         else if($type=='vendor_product')
+         {
+
+            $log = new Log('error.log');
+            $this->load->model('user/user_activity');
+
+            $activity_data = [
+                'user_id' => $this->user->getId(),
+                'name' => $this->user->getFirstName() . ' ' . $this->user->getLastName(),
+                'user_group_id' => $this->user->getGroupId(),
+                'vendor_product' => $id,
+            ];
+                 //  $log->write('product status modified');
+
+            if($status==0)
+            {
+            $this->model_user_user_activity->addActivity('vendor_product_disabled', $activity_data);
+            }
+            else{
+                $this->model_user_user_activity->addActivity('vendor_product_enabled', $activity_data);
+
+            }
+            //  $log->write('product status modified');
+         }
             }
         }
     }
