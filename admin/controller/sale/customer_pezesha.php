c34aaa010366c856662a8e00b967ab0a2f89a590
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-03-29 11:57:52 +0530

    pezesha api changes

--- admin/controller/sale/customer_pezesha.php
+++ admin/controller/sale/customer_pezesha.php
@@ -90,7 +90,7 @@ class ControllerSaleCustomerPezesha extends Controller {
         $this->response->addHeader('Content-Type: application/json');
         $this->response->setOutput(json_encode($response));
     }
-    
+
     public function applyloanfordeliveredorder() {
 
         $order_id = isset($this->request->post['order_id']) && $this->request->post['order_id'] > 0 ? $this->request->post['order_id'] : 0;
@@ -98,10 +98,12 @@ class ControllerSaleCustomerPezesha extends Controller {
         if ($order_id != NULL && $order_id > 0) {
             $this->load->model('sale/customer');
             $this->load->model('sale/order');
+            $this->load->model('pezesha/pezesha');
 
             $order_info = $this->model_sale_order->getOrder($order_id);
+            $pezesha_order_info = $this->model_pezesha_pezesha->getCustomerPezeshaLoan($order_id, $order_info['customer_id']);
 
-            if ($order_info != NULL) {
+            if ($order_info != NULL && $pezesha_order_info != NULL && $pezesha_order_info['customer_id'] == $order_info['customer_id'] && $pezesha_order_info['loan_id'] > 0 && $pezesha_order_info['order_id'] == $order_info['order_id']) {
 
                 $customer_info['customer_id'] = $order_info['customer_id'];
                 $customer_info['amount'] = $order_info['total'];
