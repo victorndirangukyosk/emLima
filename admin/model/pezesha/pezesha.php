b34244c1e3cb7e37c435ee05ed2d40804fd08d3d
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-03-31 14:49:18 +0530

    pezesha changes

--- admin/model/pezesha/pezesha.php
+++ admin/model/pezesha/pezesha.php
@@ -34,4 +34,27 @@ class ModelPezeshaPezesha extends Model {
         return $query->row;
     }
 
+    public function insertOrderTransactionId($order_id, $transaction_id, $customer_id) {
+        $log = new Log('error.log');
+        $log->write('order_id_transaction_id');
+        $log->write($order_id . ' ' . $transaction_id);
+        $log->write('order_id_transaction_id');
+        $sql = 'DELETE FROM ' . DB_PREFIX . "order_transaction_id WHERE order_id = '" . (int) $order_id . "'";
+
+        $query = $this->db->query($sql);
+
+        $sql = 'INSERT into ' . DB_PREFIX . "order_transaction_id SET order_id = '" . $order_id . "', transaction_id = '" . $transaction_id . "', customer_id = '" . $customer_id . "', created_at = NOW()";
+
+        $query = $this->db->query($sql);
+    }
+
+    public function addOrderHistory($order_id, $order_status_id, $payment_method, $payment_code, $added_by, $added_by_role, $notify, $comment) {
+        $this->db->query('UPDATE `' . DB_PREFIX . "order` SET payment_method = '" . $payment_method . "', payment_code = '" . $payment_code . "', paid = 'Y', date_modified = NOW() WHERE order_id = '" . (int) $order_id . "'");
+        $this->db->query('INSERT INTO ' . DB_PREFIX . "order_history SET order_id = '" . (int) $order_id . "', added_by = '" . (int) $added_by . "', role = '" . $added_by_role . "', order_status_id = '" . (int) $order_status_id . "', notify = '" . (int) $notify . "', comment = '" . $this->db->escape($comment) . "', date_added = NOW()");
+    }
+
+    public function addOrderHistoryFailed($order_id, $order_status_id, $payment_method, $payment_code, $added_by, $added_by_role, $notify, $comment) {
+        $this->db->query('INSERT INTO ' . DB_PREFIX . "order_history SET order_id = '" . (int) $order_id . "', added_by = '" . (int) $added_by . "', role = '" . $added_by_role . "', order_status_id = '" . (int) $order_status_id . "', notify = '" . (int) $notify . "', comment = '" . $this->db->escape($comment) . "', date_added = NOW()");
+    }
+
 }
