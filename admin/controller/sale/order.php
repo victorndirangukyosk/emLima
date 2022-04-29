7bd3c5956877f6f63f2009e34ab879867bb87278
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-04-29 10:59:00 +0530

    Update order.php

--- admin/controller/sale/order.php
+++ admin/controller/sale/order.php
@@ -3581,6 +3581,7 @@ class ControllerSaleOrder extends Controller {
             //echo "<pre>";print_r($data['products']);die;
 
             $totals = $this->model_sale_order->getOrderTotals($this->request->get['order_id']);
+            $totals_history = $this->model_sale_order->getOrderTotals_History($this->request->get['order_id']);
 
             //echo "<pre>";print_r($totals);die;
 
@@ -3588,6 +3589,18 @@ class ControllerSaleOrder extends Controller {
 
             foreach ($totals as $total) {
                 $data['totals'][] = [
+                    'title' => $total['title'],
+                    'code' => $total['code'],
+                    'text' => $this->currency->format($total['value'], $order_info['currency_code'], $order_info['currency_value']),
+                ];
+
+                if ('total' == $total['code']) {
+                    $data['total'] = $this->currency->format($total['value'], $order_info['currency_code'], $order_info['currency_value']);
+                }
+            }
+
+            foreach ($totals_history as $total) {
+                $data['totals_history'][] = [
                     'title' => $total['title'],
                     'code' => $total['code'],
                     'text' => $this->currency->format($total['value'], $order_info['currency_code'], $order_info['currency_value']),
@@ -3884,6 +3897,8 @@ class ControllerSaleOrder extends Controller {
             }
 
             //echo "<pre>";print_r($data['totals']);die;
+                // echo "<pre>";print_r($data);die;
+
             $data['payment_action'] = $this->load->controller('payment/' . $order_info['payment_code'] . '/orderAction', '');
 
             $data['header'] = $this->load->controller('common/header');
