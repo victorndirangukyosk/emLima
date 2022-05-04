7d3f82d36ee35ef1f38b01f31ead60469e4a9df9
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-05-04 14:48:40 +0530

    Create wastage_updation.php

---
+++ admin/controller/inventory/wastage_updation.php
@@ -0,0 +1,8 @@
+<?php
+
+class ControllerInventoryWastageUpdation extends Controller {
+
+    private $error = [];
+   
+    //this controller is added to create user group permissions for updating wastage 
+}
