4005b68ec0f95e301ef127b47363ece7399d0029
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-05-19 19:09:09 +0530

    Update customer.php

--- admin/controller/sale/customer.php
+++ admin/controller/sale/customer.php
@@ -3597,6 +3597,67 @@ class ControllerSaleCustomer extends Controller {
         $this->response->setOutput(json_encode($json));
     }
 
+
+    public function autocompletebyCompany_pezesha() {
+        $json = [];
+
+        if (isset($this->request->get['filter_name']) || isset($this->request->get['filter_email'])) {
+            if (isset($this->request->get['filter_name'])) {
+                $filter_name = $this->request->get['filter_name'];
+            } else {
+                $filter_name = '';
+            }
+
+            if (isset($this->request->get['filter_company'])) {
+                $filter_company = $this->request->get['filter_company'];
+            } else {
+                $filter_company = '';
+            }
+
+            $this->load->model('sale/customer');
+
+            $filter_data = [
+                'filter_name' => $filter_name,
+                'filter_company' => $filter_company,
+                'start' => 0,
+                'limit' => 5,
+            ];
+
+            $results = $this->model_sale_customer->getCustomers_pezesha($filter_data);
+
+            foreach ($results as $result) {
+                if ($this->user->isVendor()) {
+                    $result['name'] = $result['firstname'];
+                }
+
+                $json[] = [
+                    'customer_id' => $result['customer_id'],
+                    'customer_group_id' => $result['customer_group_id'],
+                    'name' => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
+                    'customer_group' => $result['customer_group'],
+                    'firstname' => $result['firstname'],
+                    'lastname' => $result['lastname'],
+                    'email' => $result['email'],
+                    'telephone' => $result['telephone'],
+                    'fax' => $result['fax'],
+                    'custom_field' => unserialize($result['custom_field']),
+                    'address' => $this->model_sale_customer->getAddresses($result['customer_id']),
+                ];
+            }
+        }
+
+        $sort_order = [];
+
+        foreach ($json as $key => $value) {
+            $sort_order[$key] = $value['name'];
+        }
+
+        array_multisort($sort_order, SORT_ASC, $json);
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
     public function customfield() {
         $json = [];
 
@@ -3802,6 +3863,44 @@ class ControllerSaleCustomer extends Controller {
         $this->response->setOutput(json_encode($json));
     }
 
+    
+    public function autocompletecompany_pezesha_all() {
+        $json = [];
+
+        if (isset($this->request->get['filter_name'])) {
+            if (isset($this->request->get['filter_name'])) {
+                $filter_name = $this->request->get['filter_name'];
+            } else {
+                $filter_name = '';
+            }
+
+            $this->load->model('sale/customer');
+
+            $filter_data = [
+                'filter_name' => $filter_name,
+                'start' => 0,
+                'limit' => 5,
+            ];
+
+            $results = $this->model_sale_customer->getCompanies_pezesha_all($filter_data);
+            foreach ($results as $result) {
+                $json[] = [
+                    'name' => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
+                ];
+            }
+        }
+
+        $sort_order = [];
+
+        foreach ($json as $key => $value) {
+            $sort_order[$key] = $value['name'];
+        }
+
+        array_multisort($sort_order, SORT_ASC, $json);
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
 
     public function autocompletepayment() {
         $json = [];
