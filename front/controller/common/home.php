2bd382efc6395fdcd454fd033b8ce59e356fab3c
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-05-27 16:33:44 +0530

    Update home.php

--- front/controller/common/home.php
+++ front/controller/common/home.php
@@ -391,6 +391,33 @@ class ControllerCommonHome extends Controller {
         //   echo "<pre>";print_r($data);die;
     }
 
+
+    public function job_opening_details_saved($id = 0, $message = "", $errormessage = "") {
+        $data['site_key'] = $this->config->get('config_google_captcha_public');
+        if (isset($this->request->get['id'])) {
+            $filter['id'] = $this->request->get['id'];
+        } else {
+            $filter['id'] = $id;
+        }
+        // echo  ($id);die;
+        // $this->load->model('information/careers');
+
+        // $data['jobpositions'] = $this->model_information_careers->getJobPositions($filter);
+        // $data['jobpositions'][0]['site_key'] = $this->config->get('config_google_captcha_public');
+        // $data['jobpositions'][0]['action'] = $this->url->link('common/home/savecareers', '', 'SSL');
+        // $data['jobpositions'][0]['message'] = $message;
+        // $data['jobpositions'][0]['errormessage'] = $errormessage;
+        // if (is_file(DIR_IMAGE . $this->config->get('config_logo'))) {
+        //     //$data['logo'] = $this->model_tool_image->resize($this->config->get('config_logo'),200,110);
+        //     $data['jobpositions'][0]['logo'] = $server . 'image/' . $this->config->get('config_logo');
+        // } else {
+        //     $data['jobpositions'][0]['logo'] = 'assets/img/logo.svg';
+        // }
+        //   echo "<pre>";print_r($data['jobpositions'][0]);die;
+        // $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/landing_page/jobopening.tpl', $data['jobpositions'][0]));
+        $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/landing_page/jobopening_saved.tpl', $data['jobpositions'][0]));
+    }
+
     public function job_opening_details($id = 0, $message = "", $errormessage = "") {
         $data['site_key'] = $this->config->get('config_google_captcha_public');
         if (isset($this->request->get['id'])) {
@@ -523,8 +550,8 @@ class ControllerCommonHome extends Controller {
         if ($this->request->post['careers-job-id'] == 0)
             $this->careers(0, $success_message, $error_message);
         else
-            $this->job_opening_details($this->request->post['careers-job-id'], $success_message, $error_message);
-        // $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/landing_page/jobopening.tpl', $data['jobpositions'][0]));
+        // $this->job_opening_details($this->request->post['careers-job-id'], $success_message, $error_message);
+        $this->job_opening_details_saved($this->request->post['careers-job-id'], $success_message, $error_message);
     }
 
     public function FeatureFileUpload($file_data) {
