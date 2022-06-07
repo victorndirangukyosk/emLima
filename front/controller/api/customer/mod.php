9229fa2d9001ab8e23f7d1a94a68860b78eeb100
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-06-07 15:24:39 +0530

    mpesa on delivery hybrid payment method

--- front/controller/api/customer/mod.php
+++ front/controller/api/customer/mod.php
@@ -55,6 +55,8 @@ class ControllerApiCustomerMod extends Controller {
             if ($customer_wallet_total > 0 && $order_info['paid'] == 'N') {
 
                 $this->load->model('sale/order');
+                $this->load->model('payment/wallet');
+
                 $totals = $this->model_sale_order->getOrderTotals($order_id);
                 $log->write($totals);
                 $total = 0;
@@ -65,11 +67,21 @@ class ControllerApiCustomerMod extends Controller {
                     }
                 }
 
-                if ($customer_info != null) {
-                    $ret = $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('mod_order_status_id'), $comment, true, $customer_info['customer_id'], 'customer');
-                } else {
-                    $ret = $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('mod_order_status_id'), $comment, true, 0, 'customer');
+                if ($customer_info != NULL && $customer_wallet_total > 0 && $totals != NULL && $total > 0 && $total <= $customer_wallet_total) {
+                    $this->model_payment_wallet->addTransactionCreditForHybridPayment($this->customer->getId(), "Wallet amount deducted #" . $order_id, $total, $order_id, 'Y', 0);
+                    $this->model_sale_order->UpdatePaymentMethod($order_id, 'Wallet Payment', 'wallet');
+                    $ret = $this->model_checkout_order->addOrderHistory($order_id, 1, 'Paid Through Wallet By Customer', FALSE, $this->customer->getId(), 'customer');
+                } elseif ($customer_info != NULL && $customer_wallet_total > 0 && $totals != NULL && $total > 0 && $total > $customer_wallet_total) {
+                    $this->model_payment_wallet->addTransactionCreditForHybridPayment($this->customer->getId(), "Wallet amount deducted #" . $value, $customer_wallet_total, $value, 'P', $customer_wallet_total);
+                    $this->model_sale_order->UpdatePaymentMethod($order_id, 'Wallet Payment', 'wallet');
+                    $ret = $this->model_checkout_order->addOrderHistory($order_id, 1, 'Paid Partially Through Wallet By Customer', FALSE, $this->customer->getId(), 'customer');
                 }
+
+                /* if ($customer_info != null) {
+                  $ret = $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('mod_order_status_id'), $comment, true, $customer_info['customer_id'], 'customer');
+                  } else {
+                  $ret = $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('mod_order_status_id'), $comment, true, 0, 'customer');
+                  } */
                 $this->load->model('account/activity');
                 $activity_data = [
                     'customer_id' => $this->customer->getId(),
