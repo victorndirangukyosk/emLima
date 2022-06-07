50928b05297186296b54bb94bd93b5c3046a6928
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-06-07 14:31:52 +0530

    mpesa on delivery hybrid payment method

--- front/controller/api/customer/mod.php
+++ front/controller/api/customer/mod.php
@@ -48,19 +48,37 @@ class ControllerApiCustomerMod extends Controller {
             $log->write('mod loop' . $order_id . 'front\controller\api\customer\mod.php');
             $log->write('mod loop' . $order_id . ' ' . $this->cart->getSubTotal() . ' ' . $this->cart->getTotal());
 
-            if ($customer_info != null) {
-                $ret = $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('mod_order_status_id'), $comment, true, $customer_info['customer_id'], 'customer');
-            } else {
-                $ret = $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('mod_order_status_id'), $comment, true, 0, 'customer');
-            }
-            $this->load->model('account/activity');
-            $activity_data = [
-                'customer_id' => $this->customer->getId(),
-                'name' => $this->customer->getFirstName() . ' ' . $this->customer->getLastName(),
-                'order_id' => $order_id,
-            ];
+            $this->load->model('account/credit');
+            $customer_wallet_total = $this->model_account_credit->getTotalAmount();
+            $order_info = $this->model_checkout_order->getOrder($order_id);
 
-            $this->model_account_activity->addActivity('order_account', $activity_data);
+            if ($customer_wallet_total > 0 && $order_info['paid'] == 'N') {
+
+                $this->load->model('sale/order');
+                $totals = $this->model_sale_order->getOrderTotals($order_id);
+                $log->write($totals);
+                $total = 0;
+                foreach ($totals as $total) {
+                    if ('total' == $total['code']) {
+                        $total = $total['value'];
+                        break;
+                    }
+                }
+
+                if ($customer_info != null) {
+                    $ret = $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('mod_order_status_id'), $comment, true, $customer_info['customer_id'], 'customer');
+                } else {
+                    $ret = $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('mod_order_status_id'), $comment, true, 0, 'customer');
+                }
+                $this->load->model('account/activity');
+                $activity_data = [
+                    'customer_id' => $this->customer->getId(),
+                    'name' => $this->customer->getFirstName() . ' ' . $this->customer->getLastName(),
+                    'order_id' => $order_id,
+                ];
+
+                $this->model_account_activity->addActivity('order_account', $activity_data);
+            }
         }
     }
 
