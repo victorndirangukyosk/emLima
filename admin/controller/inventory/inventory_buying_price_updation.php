f5999a63ed17d918966dc1a982a7565b0587f1c7
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-06-09 15:13:03 +0530

    Create inventory_buying_price_updation.php

---
+++ admin/controller/inventory/inventory_buying_price_updation.php
@@ -0,0 +1,9 @@
+<?php
+
+class ControllerInventoryInventoryBuyingPriceUpdation extends Controller {
+
+    private $error = [];
+   
+    //this controller is added to create user group permissions for updating buyingPrice of inventory
+
+}
