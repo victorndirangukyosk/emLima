c1f3310cdd17ad4d5106482bcc0f9d4899e89bad
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-06-14 18:10:49 +0530

    wallet payment method changes

--- front/controller/api/customer/wallet.php
+++ front/controller/api/customer/wallet.php
@@ -469,8 +469,14 @@ class ControllerApiCustomerWallet extends Controller {
 
         $this->load->model('checkout/order');
 
-        $log->write($this->config->get('wallet_order_status_id'));
+        /* DECIDING ORDER STATUS IF CUSTOMER SUB CUSTOMER */
+        $this->load->model('account/customer');
         $is_he_parents = $this->model_account_customer->CheckHeIsParent();
+        $log->write('Order Confirm In Mobile WALLET');
+        $log->write($is_he_parents);
+        $log->write($this->customer->getOrderApprovalAccess());
+        $log->write($this->customer->getOrderApprovalAccessRole());
+        $log->write('Order Confirm In Mobile WALLET');
 
         $parent_customer_info = NULL;
         if ($is_he_parents != NULL && $is_he_parents > 0) {
@@ -479,29 +485,19 @@ class ControllerApiCustomerWallet extends Controller {
 
         $sub_customer_order_approval_required = 1;
         if (isset($parent_customer_info) && $parent_customer_info != NULL && is_array($parent_customer_info)) {
-            // $sub_customer_order_approval_required = $parent_customer_info['sub_customer_order_approval'];
             $sub_customer_info = $this->model_account_customer->getCustomer($this->customer->getId());
             $sub_customer_order_approval_required = $sub_customer_info['sub_customer_order_approval'];
         }
 
         $order_appoval_access = FALSE;
-        //session values are not getting
-        // if ($this->session->data['order_approval_access'] > 0 && $this->session->data['order_approval_access_role'] != NULL) {
-        //     $order_appoval_access = TRUE;
-        // }
-
-        $Approver_assigned = $this->model_account_customer->CheckApprover();
-        $log->write($Approver_assigned['order_approval_access']);
-        $log->write($Approver_assigned['order_approval_access_role']);
-
-        if ($Approver_assigned['order_approval_access'] > 0 && $Approver_assigned['order_approval_access_role'] != NULL) {
+        if ($this->customer->getOrderApprovalAccess() > 0 && $this->customer->getOrderApprovalAccessRole() != NULL) {
             $order_appoval_access = TRUE;
         }
 
-
-
         $parent_approval = $is_he_parents == NULL || $order_appoval_access == TRUE || $sub_customer_order_approval_required == 0 ? 'Approved' : 'Pending';
         $order_status_id = $is_he_parents == NULL || $order_appoval_access == TRUE || $sub_customer_order_approval_required == 0 ? $this->config->get('wallet_order_status_id') : 15;
+        $order_status_id = $order_status_id > 0 ? $order_status_id : $this->config->get('mod_order_status_id');
+        /* DECIDING ORDER STATUS IF CUSTOMER SUB CUSTOMER */
 
         $order_id = NULL;
         foreach ($orders as $order_id) {
@@ -514,13 +510,24 @@ class ControllerApiCustomerWallet extends Controller {
         foreach ($orders as $order_id) {
             $log->write('wallet loop' . $order_id);
 
-            // $ret = $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('wallet_order_status_id'));
-            // $ret = $this->model_checkout_order->addOrderHistory($order_id, $order_status_id);
-            if ($customer_info != null)
-                $ret = $this->model_checkout_order->addOrderHistory($order_id, $order_status_id, $comment, true, $customer_info['customer_id'], 'customer');
-            else
-                $ret = $this->model_checkout_order->addOrderHistory($order_id, $order_status_id, $comment, true, 0, 'customer');
+            $this->load->model('sale/order');
 
+            $totals = $this->model_sale_order->getOrderTotals($order_id);
+            $log->write($totals);
+            $total = 0;
+            foreach ($totals as $total) {
+                if ('total' == $total['code']) {
+                    $total = $total['value'];
+                    break;
+                }
+            }
+
+            if ($customer_info != null) {
+                $this->model_checkout_order->addTransactionCredit($this->customer->getId(), 'Paid Through Wallet By Customer For #' . $order_id, $total, $order_id);
+                $ret = $this->model_checkout_order->addOrderHistory($order_id, $order_status_id, $comment, true, $customer_info['customer_id'], 'customer');
+            } else {
+                $ret = $this->model_checkout_order->addOrderHistory($order_id, $order_status_id, $comment, true, 0, 'customer');
+            }
             $this->load->model('account/activity');
             $activity_data = [
                 'customer_id' => $this->customer->getId(),
@@ -530,10 +537,6 @@ class ControllerApiCustomerWallet extends Controller {
 
             $this->model_account_activity->addActivity('order_account', $activity_data);
         }
-
-        /* if ($order_id != NULL) {
-          $this->model_checkout_order->UpdateParentApprovalAPI($order_id, $Approver_assigned['order_approval_access'], $Approver_assigned['order_approval_access_role']);
-          } */
 
         if ($parent_approval == "Pending") {//&& $sub_customer_order_approval_required == 1
             $this->load->model('checkout/order');
