d0a65e65313d37ac1744cfb8aab8541056baced6
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-07-06 19:47:07 +0530

    Update customer.php

--- system/library/customer.php
+++ system/library/customer.php
@@ -107,9 +107,23 @@ class Customer {
 
     public function login($email, $password, $override = false) {
         if ($override) {
-            $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "' AND status = '1'");
+            if(is_numeric($email))//if numeric , telephone ,need to consider
+            {
+            $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE (telephone) = '" . $this->db->escape(($email)) . "' AND status = '1'");
+            }
+            else{
+                $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "' AND status = '1'");
+
+            }
         } else {
-            $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "' AND (password = SHA1(CONCAT(salt, SHA1(CONCAT(salt, SHA1('" . $this->db->escape($password) . "'))))) OR password = '" . $this->db->escape(md5($password)) . "') AND status = '1' AND approved = '1'");
+            if(is_numeric($email))
+            {
+            $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE (telephone) = '" . $this->db->escape(($email)) . "' AND (password = SHA1(CONCAT(salt, SHA1(CONCAT(salt, SHA1('" . $this->db->escape($password) . "'))))) OR password = '" . $this->db->escape(md5($password)) . "') AND status = '1' AND approved = '1'");
+            }
+            else{
+                $customer_query = $this->db->query('SELECT * FROM ' . DB_PREFIX . "customer WHERE LOWER(email) = '" . $this->db->escape(utf8_strtolower($email)) . "' AND (password = SHA1(CONCAT(salt, SHA1(CONCAT(salt, SHA1('" . $this->db->escape($password) . "'))))) OR password = '" . $this->db->escape(md5($password)) . "') AND status = '1' AND approved = '1'");
+
+            }
         }
 
         if ($customer_query->num_rows) {
