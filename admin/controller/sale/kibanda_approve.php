ec2ead21f9c224b8cb5701e9db6f6305b09524ef
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-07-22 11:43:03 +0530

    seperate permissions for kibanda

---
+++ admin/controller/sale/kibanda_approve.php
@@ -0,0 +1,17 @@
+<?php
+
+class ControllerSaleKibandaApprove extends Controller {
+
+    private $error = [];
+    //added for giving permission to controller /approve tab,but code didnt moved from kibanda.php
+  
+
+    public function index() {//kibanda_approve
+       
+    }
+
+  
+   
+ 
+
+}
---
+++ admin/controller/sale/kibanda_login.php
@@ -0,0 +1,13 @@
+
+
+
+<?php
+
+class ControllerSaleKibandaLogin extends Controller {
+
+    private $error = [];// login as Kibanda customer  button
+
+   
+ 
+
+}
