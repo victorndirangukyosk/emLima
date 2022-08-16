279e2ef8d7f8cc6f0eb60c46ce3752e280dbe0e1
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-08-16 16:03:14 +0530

    excel date type in cell

--- admin/language/english/english.php
+++ admin/language/english/english.php
@@ -7,6 +7,7 @@ $_['code'] = 'en';
 $_['direction'] = 'ltr';
 $_['date_format_short'] = 'd-m-Y';
 $_['date_format_short_report'] = 'd-m-Y';
+// $_['date_format_short_report'] = 'dd-mm-yyyy';
 $_['date_format_long'] = 'l dS F Y';
 $_['date_format_medium'] = 'dS F Y';
 $_['time_format'] = 'h:i:s A';
