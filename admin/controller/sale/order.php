083e7dc0514f58e024b03d38cb18c9db83558f87
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-09-27 20:36:18 +0530

    NA Flag

--- admin/controller/sale/order.php
+++ admin/controller/sale/order.php
@@ -5064,7 +5064,7 @@ class ControllerSaleOrder extends Controller {
                 if ($order_customer_detials != NULL && is_array($order_customer_detials)) {
                     $order_customer_first_last_name = $order_customer_detials['firstname'] . ' ' . $order_customer_detials['lastname'];
                     $company_name = $order_customer_detials['company_name'];
-                    $paybill_act = $order_customer_detials['paybill_act'];
+                    $paybill_act = $order_customer_detials['paybill_act']??'NA';
                     $customer_account_manager_detials = $this->model_user_accountmanager->getUser($order_customer_detials['account_manager_id']);
                     if ($order_customer_detials['account_manager_id'] > 0 && $order_customer_detials['account_manager_id'] != NULL && $customer_account_manager_detials != NULL) {
                         $customer_account_manager_first_last_name = $customer_account_manager_detials['firstname'] . ' ' . $customer_account_manager_detials['lastname'];
@@ -6115,7 +6115,7 @@ class ControllerSaleOrder extends Controller {
                     if ($order_customer_detials != NULL && is_array($order_customer_detials)) {
                         $order_customer_first_last_name = $order_customer_detials['firstname'] . ' ' . $order_customer_detials['lastname'];
                         $company_name = $order_customer_detials['company_name'];
-                        $paybill_act = $order_customer_detials['paybill_act'];
+                        $paybill_act = $order_customer_detials['paybill_act']??'NA';
                     }
 
                     $this->load->model('drivers/drivers');
