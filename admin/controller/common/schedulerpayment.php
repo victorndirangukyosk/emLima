a0ade07d1b51f3b835241ebc287e891a38f495a1
Sri Divya <sridivya.talluri@technobraingroup.com>
2022-09-29 16:07:32 +0530

    Update schedulerpayment.php

--- admin/controller/common/schedulerpayment.php
+++ admin/controller/common/schedulerpayment.php
@@ -12,14 +12,15 @@ class ControllerCommonSchedulerPayment extends Controller {
     public function UpdatePaybillPayments() {
         $log = new Log('error.log');
         try {
-            $log->write('UpdatePaybillPayments -' . Now());
+            $log->write('UpdatePaybillPayments -' . date('Y-m-d H:i:s'));
             $this->load->model('scheduler/dbupdates');
             //Get unallocated Funds Customers
             $result_custs = $this->model_scheduler_dbupdates->GetUnallocatedFundCustomers();
-            // echo "<pre>";print_r($result);die; 
+            // echo "<pre>";print_r($result_custs);die; 
             foreach ($result_custs as $cust) {
                 //get oending orders in FIFO (deivery date ASC)
-                $result_orders=$this->model_scheduler_dbupdates->GetCustomerPendingOrders($cust);
+                $result_orders=$this->model_scheduler_dbupdates->GetCustomerPendingOrders($cust['customer_id']);
+                // echo "<pre>";print_r($result_orders);die; 
                 
                 $order_total_pending_Value = 0;
                 foreach ($result_orders as $ord) {
@@ -36,48 +37,53 @@ class ControllerCommonSchedulerPayment extends Controller {
 
                     // }
                     //funds fetching should be in inner for loop, because funds balance will be updated based on order totals
-                     $result_funds = $this->model_scheduler_dbupdates->GetUnallocatedFunds($cust);
+                     $result_funds = $this->model_scheduler_dbupdates->GetUnallocatedFunds($cust['customer_id']);
+                    //  echo "<pre>";print_r($result_funds);die; 
 
                     $closed=0;
                     $order_paid=0;
                     $ord['pending_total']=$ord['total']-$ord['amount_partialy_paid'];
                     foreach ($result_funds as $fund) {
-                     if( ($fund['amount']==$ord['pending_total']) && $order_paid==0)
+                     if( ($fund['available_balance']==$ord['pending_total']) && $order_paid==0)
                      {//update order to paid and deduct from funds_totls
                         //and finally map the fund id as closed/totaly used,reset pending_amount
-                        $this->model_scheduler_dbupdates->confirmPaymentReceived($ord['order_id'], $fund['transaction_id'], $fund['amount'],0, 'Pay bill No',$ord['pending_total'],$ord['pending_total']);
+                        $this->model_scheduler_dbupdates->confirmPaymentReceived($ord['order_id'], $fund['transaction_id'], $fund['amount'],0, 'Pay bill No',$ord['pending_total'],$fund['available_balance']);
                         $order_paid=1;
                         $closed=1;     
                         $available_balance=0;
-                        $amount_used   =  $fund['amount']    ;                    
-                        $this->model_scheduler_dbupdates->updateFundAndTotals($fund['customer_fund_id'], $closed,$available_balance,$amount_used);
+                        $amount_used   =  $fund['available_balance']    ;                    
+                        $this->model_scheduler_dbupdates->updateFundAndTotals($fund['customer_fund_id'], $closed,$available_balance,$amount_used,$cust['customer_id']);
                             
 
                      }
-                     else if(($fund['amount']>$ord['pending_total']) && $order_paid==0 )
+                     else if(($fund['available_balance']>$ord['pending_total']) && $order_paid==0 )
                      {// deduct from funds_totals
                         //and finally map the fund id as closed/totaly used,
                         $this->model_scheduler_dbupdates->confirmPaymentReceived($ord['order_id'], $fund['transaction_id'], $fund['amount'],0, 'Pay bill No',$ord['pending_total'],$ord['pending_total']);
 
-                        $order_paid=1;$closed=1; 
-                        $available_balance=$fund['amount']-$ord['pending_total'];
-                        $amount_used   =  $ord['pending_total']   ;                    
-                        $this->model_scheduler_dbupdates->updateFundAndTotals($fund['customer_fund_id'], $closed,$available_balance,$amount_used);
+                        $order_paid=1;$closed=0; 
+                        $available_balance=$fund['available_balance']-$ord['pending_total'];
+                        $amount_used   =  $fund['amount_used']+$ord['pending_total']   ;                    
+                        $this->model_scheduler_dbupdates->updateFundAndTotals($fund['customer_fund_id'], $closed,$available_balance,$amount_used,$cust['customer_id']);
                         //extra amount adding to wallet is in discussion
                        //even after discussion, write seperate job for crediting amount, as forloop is using
                         //and need to run that job , once the payment job is completed
 
                      }
-                     else if($fund['amount']<$ord['pending_total'] && $order_paid==0 )
+                     else if($fund['available_balance']<$ord['pending_total'] && $order_paid==0 )
                      {//update order to partialy paid and
-                        $this->model_scheduler_dbupdates->confirmPartialPaymentReceived($ord['order_id'], $fund['transaction_id'], $fund['amount'],0, 'Pay bill No',$ord['pending_total'],$ord['pending_total']);
+                       
+                        $amount_partialy_paid=$fund['available_balance']+ord['amount_partialy_paid'];
+                       
+                        $this->model_scheduler_dbupdates->confirmPartialPaymentReceived($ord['order_id'], $fund['transaction_id'], $fund['amount'],$amount_partialy_paid, 'Pay bill No',$ord['pending_total'],$fund['available_balance']);
 
                         //update available balance and amount_used
                         $order_paid=0;
-                        $closed=0;
-                        // $available_balance=$fund['amount']-$ord['pending_total'];
-                        $amount_used   =  $fund['amount']   ;                    
-                        $this->model_scheduler_dbupdates->updateFundAndTotals($fund['customer_fund_id'], $closed,$available_balance,$amount_used);
+                        $closed=1;
+                        // $available_balance=$fund['available_balance']-$fund['available_balance'];
+                        $available_balance=0;
+                        $amount_used   =  $fund['amount_used']+$fund['available_balance']   ;                    
+                        $this->model_scheduler_dbupdates->updateFundAndTotals($fund['customer_fund_id'], $closed,$available_balance,$amount_used,$cust['customer_id']);
                         
                      }
                     }              
