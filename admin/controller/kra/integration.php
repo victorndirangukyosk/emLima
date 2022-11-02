07cba6bcb39f869aa665a287582fb7dedbccda80
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-11-02 10:52:22 +0530

    kra integration direct apis

--- admin/controller/kra/integration.php
+++ admin/controller/kra/integration.php
@@ -13,4 +13,39 @@ class ControllerKraIntegration extends Controller {
         return $response;
     }
 
+    public function settings() {
+
+        $log = new Log('error.log');
+
+        $com = $this->request->post['com'];
+        $baud = $this->request->post['baud'];
+        $tcp = $this->request->post['tcp'];
+        $ip = $this->request->post['ip'];
+        $port = $this->request->post['port'];
+        $password = $this->request->post['password'];
+
+        $settings = "(com=" . $com . ",baud=" . $baud . ",tcp=" . $tcp . ",ip=" . $ip . ",port=" . $port . ",password=" . $password . ")";
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'http://localhost:4444/Settings' . $settings);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/x-www-form-urlencoded'));
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POST, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        $result = curl_exec($curl);
+        $xml_snippet = simplexml_load_string($result);
+        $json_convert = json_encode($xml_snippet);
+
+        $log->write($result);
+        curl_close($curl);
+        $final_result = json_decode($json_convert, true);
+        $json['status'] = true;
+        $json['data'] = $final_result;
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+
 }
